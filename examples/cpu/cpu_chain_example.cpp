9680c1a739b5a57984b37b01c755f04b93725748
asalzburger <Andreas.Salzburger@cern.ch>
2021-01-30 12:58:37 +0100

    adding file header

--- examples/cpu/cpu_chain_example.cpp
+++ examples/cpu/cpu_chain_example.cpp
@@ -1,3 +1,10 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ * 
+ * (c) 2021 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
 #include <taskflow/taskflow.hpp>  
 
 int main(){
