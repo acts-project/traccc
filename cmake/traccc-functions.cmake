1d2e7db065913276fb34d5d0b07128fb232cf484
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-04-12 09:37:35 +0200

    Taught the project how to use (and possibly build) VecMem.
    Tidied up the CMake configuration a bit, making sure that the settings
    made in traccc could be in sync with the settings made in VecMem.
    
    Introduced a couple of helper CMake modules for this, under the cmake/
    subdirectory. This includes traccc-vecmem.cmake, which takes care of
    either finding, or building VecMem (in case it was not found).
    
    Updated the traccc::core library to "link against" vecmem::core.

---
+++ cmake/traccc-functions.cmake
@@ -0,0 +1,30 @@
+# TRACCC library, part of the ACTS project (R&D line)
+#
+# (c) 2021 CERN for the benefit of the ACTS project
+#
+# Mozilla Public License Version 2.0
+
+# Guard against multiple includes.
+include_guard( GLOBAL )
+
+# Helper function for adding individual flags to "flag variables".
+#
+# Usage: traccc_add_flag( CMAKE_CXX_FLAGS "-Wall" )
+#
+function( traccc_add_flag name value )
+
+   # Escape special characters in the value:
+   set( matchedValue "${value}" )
+   foreach( c "*" "." "^" "$" "+" "?" )
+      string( REPLACE "${c}" "\\${c}" matchedValue "${matchedValue}" )
+   endforeach()
+
+   # Check if the variable already has this value in it:
+   if( "${${name}}" MATCHES "${matchedValue}" )
+      return()
+   endif()
+
+   # If not, then let's add it now:
+   set( ${name} "${${name}} ${value}" PARENT_SCOPE )
+
+endfunction( traccc_add_flag )
---
+++ cmake/traccc-vecmem.cmake
@@ -0,0 +1,40 @@
+# TRACCC library, part of the ACTS project (R&D line)
+#
+# (c) 2021 CERN for the benefit of the ACTS project
+#
+# Mozilla Public License Version 2.0
+
+# Guard against multiple includes.
+include_guard( GLOBAL )
+
+# Look for VecMem, quietly first time around. Since if it is not found (which is
+# likely), it prints multiple lines of warnings.
+find_package( vecmem QUIET )
+
+# If it was found, then we're finished.
+if( vecmem_FOUND )
+   # Call find_package again, just to nicely print where it is picked up from.
+   find_package( vecmem )
+   return()
+endif()
+
+# CMake include(s).
+cmake_minimum_required( VERSION 3.11 )
+include( FetchContent )
+
+# Tell the user what's happening.
+message( STATUS "Building VecMem as part of the traccc project" )
+
+# Declare where to get VecMem from.
+FetchContent_Declare( VecMem
+   GIT_REPOSITORY "https://github.com/acts-project/vecmem.git"
+   GIT_TAG "2e06a5167e0927e6e6ed6a4f1abd5b5f552f059f" )
+
+# Prevent VecMem from building its tests. As it would interfere with how traccc
+# builds/uses GoogleTest.
+set( BUILD_TESTING FALSE )
+
+# Get it into the current directory.
+FetchContent_Populate( VecMem )
+add_subdirectory( "${vecmem_SOURCE_DIR}" "${vecmem_BINARY_DIR}"
+   EXCLUDE_FROM_ALL )
