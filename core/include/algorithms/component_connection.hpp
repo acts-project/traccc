ec63b55081875780e24a05bec5734a0940039543
Paul Gessinger <hello@paulgessinger.com>
2021-06-15 13:07:00 +0200

    apply clang-format

--- core/include/algorithms/component_connection.hpp
+++ core/include/algorithms/component_connection.hpp
@@ -7,128 +7,128 @@
 
 #pragma once
 
+#include "detail/sparse_ccl.hpp"
 #include "edm/cell.hpp"
 #include "edm/cluster.hpp"
-#include "detail/sparse_ccl.hpp"
 
 namespace traccc {
 
-    /// Connected component labelling
+/// Connected component labelling
+///
+/// Note that the separation between the public and private interface is
+/// only there in the class because the compilers can't automatically figure
+/// out the "vector type" of the templated implementation, without adding a
+/// lot of "internal knowledge" about the vector types into this piece of
+/// code. So instead the public operators are specifically implemented for
+/// the host- and device versions of the EDM, making use of a single
+/// implementation internally.
+///
+class component_connection {
+   public:
+    /// @name Operators to use in host code
+    /// @{
+
+    /// Callable operator for the connected component, based on one single
+    /// module
     ///
-    /// Note that the separation between the public and private interface is
-    /// only there in the class because the compilers can't automatically figure
-    /// out the "vector type" of the templated implementation, without adding a
-    /// lot of "internal knowledge" about the vector types into this piece of
-    /// code. So instead the public operators are specifically implemented for
-    /// the host- and device versions of the EDM, making use of a single
-    /// implementation internally.
+    /// @param cells are the input cells into the connected component, they are
+    ///              per module and unordered
+    /// @param module The description of the module that the cells belong to
     ///
-    class component_connection {
+    /// c++20 piping interface:
+    /// @return a cluster collection
+    ///
+    cluster_collection operator()(const host_cell_collection& cells,
+                                  const cell_module& module) const {
+        return this->operator()<vecmem::vector>(cells, module);
+    }
 
-    public:
-        /// @name Operators to use in host code
-        /// @{
+    /// Callable operator for the connected component, based on one single
+    /// module
+    ///
+    /// @param cells are the input cells into the connected component, they are
+    ///              per module and unordered
+    /// @param module The description of the module that the cells belong to
+    /// @param clusters[in,out] are the output clusters
+    ///
+    /// void interface
+    ///
+    void operator()(const host_cell_collection& cells,
+                    const cell_module& module,
+                    cluster_collection& clusters) const {
+        this->operator()<vecmem::vector>(cells, module, clusters);
+    }
 
-        /// Callable operator for the connected component, based on one single module
-        ///
-        /// @param cells are the input cells into the connected component, they are
-        ///              per module and unordered
-        /// @param module The description of the module that the cells belong to
-        ///
-        /// c++20 piping interface:
-        /// @return a cluster collection
-        ///
-        cluster_collection operator()(const host_cell_collection& cells,
-                                      const cell_module& module) const {
+    /// @}
 
-            return this->operator()< vecmem::vector >( cells, module );
-        }
+    /// @name Operators to use in device code
+    /// @{
 
-        /// Callable operator for the connected component, based on one single module
-        ///
-        /// @param cells are the input cells into the connected component, they are
-        ///              per module and unordered
-        /// @param module The description of the module that the cells belong to
-        /// @param clusters[in,out] are the output clusters
-        ///
-        /// void interface
-        ///
-        void operator()(const host_cell_collection& cells,
-                        const cell_module& module,
-                        cluster_collection& clusters) const {
+    /// Callable operator for the connected component, based on one single
+    /// module
+    ///
+    /// This version of the function is meant to be used in device code.
+    ///
+    /// @param cells are the input cells into the connected component, they are
+    ///              per module and unordered
+    /// @param module The description of the module that the cells belong to
+    ///
+    /// c++20 piping interface:
+    /// @return a cluster collection
+    ///
+    cluster_collection operator()(const device_cell_collection& cells,
+                                  const cell_module& module) const {
+        return this->operator()<vecmem::device_vector>(cells, module);
+    }
 
-            this->operator()< vecmem::vector >( cells, module, clusters );
-        }
+    /// Callable operator for the connected component, based on one single
+    /// module
+    ///
+    /// @param cells are the input cells into the connected component, they are
+    ///              per module and unordered
+    /// @param module The description of the module that the cells belong to
+    /// @param clusters[in,out] are the output clusters
+    ///
+    /// void interface
+    ///
+    void operator()(const device_cell_collection& cells,
+                    const cell_module& module,
+                    cluster_collection& clusters) const {
+        this->operator()<vecmem::device_vector>(cells, module, clusters);
+    }
 
-        /// @}
+   private:
+    /// Implementation for the public cell collection creation operators
+    template <template <typename> class vector_type>
+    cluster_collection operator()(const cell_collection<vector_type>& cells,
+                                  const cell_module& module) const {
+        cluster_collection clusters;
+        clusters.placement = module.placement;
+        this->operator()<vector_type>(cells, module, clusters);
+        return clusters;
+    }
 
-        /// @name Operators to use in device code
-        /// @{
-
-        /// Callable operator for the connected component, based on one single module
-        ///
-        /// This version of the function is meant to be used in device code.
-        ///
-        /// @param cells are the input cells into the connected component, they are
-        ///              per module and unordered
-        /// @param module The description of the module that the cells belong to
-        ///
-        /// c++20 piping interface:
-        /// @return a cluster collection
-        ///
-        cluster_collection operator()(const device_cell_collection& cells,
-                                      const cell_module& module) const {
-
-            return this->operator()< vecmem::device_vector >( cells, module );
-        }
-
-        /// Callable operator for the connected component, based on one single module
-        ///
-        /// @param cells are the input cells into the connected component, they are
-        ///              per module and unordered
-        /// @param module The description of the module that the cells belong to
-        /// @param clusters[in,out] are the output clusters
-        ///
-        /// void interface
-        ///
-        void operator()(const device_cell_collection& cells,
-                        const cell_module& module,
-                        cluster_collection& clusters) const {
-
-            this->operator()< vecmem::device_vector >( cells, module, clusters );
-        }
-
-    private:
-        /// Implementation for the public cell collection creation operators
-        template< template< typename > class vector_type >
-        cluster_collection operator()(const cell_collection< vector_type >& cells,
-                                      const cell_module& module) const {
-            cluster_collection clusters;
-            clusters.placement = module.placement;
-            this->operator()< vector_type >(cells, module, clusters);
-            return clusters;
-        }
-
-        /// Implementation for the public cell collection creation operators
-        template< template< typename > class vector_type >
-        void operator()(const cell_collection< vector_type >& cells,
-                        const cell_module& module,
-                        cluster_collection& clusters) const {
-            // Assign the module id
-            clusters.module = module.module;
-            // Run the algorithm
-            auto connected_cells = detail::sparse_ccl<vector_type>(cells);
-            std::vector<cluster> cluster_items(std::get<0>(connected_cells),cluster{});
-            unsigned int icell = 0;
-            for (auto cell_label : std::get<1>(connected_cells)){
-                auto cindex = static_cast<unsigned int>(cell_label-1);
-                if (cindex < cluster_items.size()){
-                    cluster_items[cindex].cells.push_back(cells[icell++]);
-                }
+    /// Implementation for the public cell collection creation operators
+    template <template <typename> class vector_type>
+    void operator()(const cell_collection<vector_type>& cells,
+                    const cell_module& module,
+                    cluster_collection& clusters) const {
+        // Assign the module id
+        clusters.module = module.module;
+        // Run the algorithm
+        auto connected_cells = detail::sparse_ccl<vector_type>(cells);
+        std::vector<cluster> cluster_items(std::get<0>(connected_cells),
+                                           cluster{});
+        unsigned int icell = 0;
+        for (auto cell_label : std::get<1>(connected_cells)) {
+            auto cindex = static_cast<unsigned int>(cell_label - 1);
+            if (cindex < cluster_items.size()) {
+                cluster_items[cindex].cells.push_back(cells[icell++]);
             }
-            clusters.items = cluster_items;
         }
+        clusters.items = cluster_items;
+    }
 
-    }; // class component_connection
+};  // class component_connection
 
-} // namespace traccc
+}  // namespace traccc
--- core/include/edm/measurement.hpp
+++ core/include/edm/measurement.hpp
@@ -1,47 +1,56 @@
 /** TRACCC library, part of the ACTS project (R&D line)
- * 
+ *
  * (c) 2021 CERN for the benefit of the ACTS project
- * 
+ *
  * Mozilla Public License Version 2.0
  */
 
 #pragma once
 
-#include "container.hpp"
-#include "definitions/primitives.hpp"
-#include "definitions/algebra.hpp"
 #include <vector>
 
+#include "container.hpp"
+#include "definitions/algebra.hpp"
+#include "definitions/primitives.hpp"
+
 namespace traccc {
-    
-    /// A measurement definition:
-    /// fix to two-dimensional here
-    struct measurement {
-        point2 local = {0., 0.};
-        variance2 variance = { 0., 0.};
-    };
 
-    /// Container of measurements belonging to one detector module
-    template< template< typename > class vector_t >
-    using measurement_collection = vector_t< measurement >;
+/// A measurement definition:
+/// fix to two-dimensional here
+struct measurement {
+    point2 local = {0., 0.};
+    variance2 variance = {0., 0.};
+};
 
-    /// Convenience declaration for the measurement collection type to use in host code
-    using host_measurement_collection = measurement_collection< vecmem::vector >;
-    /// Convenience declaration for the measurement collection type to use in device code
-    using device_measurement_collection = measurement_collection< vecmem::device_vector >;
+/// Container of measurements belonging to one detector module
+template <template <typename> class vector_t>
+using measurement_collection = vector_t<measurement>;
 
-    /// Convenience declaration for the measurement container type to use in host code
-    using host_measurement_container = host_container< cell_module, measurement >;
+/// Convenience declaration for the measurement collection type to use in host
+/// code
+using host_measurement_collection = measurement_collection<vecmem::vector>;
+/// Convenience declaration for the measurement collection type to use in device
+/// code
+using device_measurement_collection =
+    measurement_collection<vecmem::device_vector>;
 
-    /// Convenience declaration for the measurement container type to use in device code    
-    using device_measurement_container = device_container< cell_module, measurement >;
-    /// Convenience declaration for the measurement container data type to use in host code
-    using measurement_container_data = container_data< cell_module, measurement >;
+/// Convenience declaration for the measurement container type to use in host
+/// code
+using host_measurement_container = host_container<cell_module, measurement>;
 
-    /// Convenience declaration for the measurement container buffer type to use in host code    
-    using measurement_container_buffer = container_buffer< cell_module, measurement >;
+/// Convenience declaration for the measurement container type to use in device
+/// code
+using device_measurement_container = device_container<cell_module, measurement>;
+/// Convenience declaration for the measurement container data type to use in
+/// host code
+using measurement_container_data = container_data<cell_module, measurement>;
 
-    /// Convenience declaration for the measurement container view type to use in host code    
-    using measurement_container_view = container_view< cell_module, measurement >;
-    
-}
+/// Convenience declaration for the measurement container buffer type to use in
+/// host code
+using measurement_container_buffer = container_buffer<cell_module, measurement>;
+
+/// Convenience declaration for the measurement container view type to use in
+/// host code
+using measurement_container_view = container_view<cell_module, measurement>;
+
+}  // namespace traccc
