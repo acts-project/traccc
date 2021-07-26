317bcf4ce3ddda83cd995c21703f6ce5f0bced80
Georgiana Mania <georgiana.mania@desy.de>
2021-07-26 12:52:44 +0200

    format

--- examples/openmp/io_decoupled/io_dec_par_example.cpp
+++ examples/openmp/io_decoupled/io_dec_par_example.cpp
@@ -1,16 +1,17 @@
+#include <chrono>
+#include <iostream>
+
 #include "algorithms/component_connection.hpp"
 #include "algorithms/measurement_creation.hpp"
 #include "algorithms/spacepoint_formation.hpp"
 #include "edm/measurement.hpp"
 #include "edm/spacepoint.hpp"
 #include "reader.hpp"
-#include "writer.hpp"
 #include "tmp_edm.hpp"
+#include "writer.hpp"
 
-#include <iostream>
-#include <chrono>
-
-traccc::demonstrator_result run(traccc::demonstrator_input input_data, vecmem::host_memory_resource resource) {
+traccc::demonstrator_result run(traccc::demonstrator_input input_data,
+                                vecmem::host_memory_resource resource) {
 
     // Algorithms
     traccc::component_connection cc;
@@ -26,11 +27,13 @@ traccc::demonstrator_result run(traccc::demonstrator_input input_data, vecmem::h
     uint64_t n_measurements = 0;
     uint64_t n_space_points = 0;
 
-    traccc::demonstrator_result aggregated_results(input_data.size(), &resource);
+    traccc::demonstrator_result aggregated_results(input_data.size(),
+                                                   &resource);
 
 #pragma omp parallel for reduction(+:n_modules, n_cells, n_clusters, n_measurements, n_space_points)
     for (size_t event = 0; event < input_data.size(); ++event) {
-        traccc::host_cell_container cells_per_event = input_data.operator[](event);
+        traccc::host_cell_container cells_per_event =
+            input_data.operator[](event);
 
         traccc::host_measurement_container measurements_per_event;
         traccc::host_spacepoint_container spacepoints_per_event;
@@ -42,17 +45,18 @@ traccc::demonstrator_result run(traccc::demonstrator_input input_data, vecmem::h
 #pragma omp parallel for
         for (size_t i = 0; i < cells_per_event.items.size(); ++i) {
             auto &module = cells_per_event.headers[i];
-            module.pixel = traccc::pixel_segmentation{-8.425, -36.025, 0.05, 0.05};
+            module.pixel =
+                traccc::pixel_segmentation{-8.425, -36.025, 0.05, 0.05};
 
             // The algorithmic code part: start
             traccc::cluster_collection clusters_per_module =
-                    cc({cells_per_event.items[i], cells_per_event.headers[i]});
+                cc({cells_per_event.items[i], cells_per_event.headers[i]});
             clusters_per_module.position_from_cell = module.pixel;
 
             traccc::host_measurement_collection measurements_per_module =
-                    mt({clusters_per_module, module});
+                mt({clusters_per_module, module});
             traccc::host_spacepoint_collection spacepoints_per_module =
-                    sp({module, measurements_per_module});
+                sp({module, measurements_per_module});
             // The algorithmnic code part: end
 
             n_cells += cells_per_event.items[i].size();
@@ -63,31 +67,34 @@ traccc::demonstrator_result run(traccc::demonstrator_input input_data, vecmem::h
 #pragma omp critical
             {
                 measurements_per_event.items.push_back(
-                        std::move(measurements_per_module));
+                    std::move(measurements_per_module));
                 measurements_per_event.headers.push_back(module);
 
                 spacepoints_per_event.items.push_back(
-                        std::move(spacepoints_per_module));
+                    std::move(spacepoints_per_module));
                 spacepoints_per_event.headers.push_back(module.module);
             }
         }
 
-        aggregated_results[event] = traccc::result({measurements_per_event, spacepoints_per_event});
-
+        aggregated_results[event] =
+            traccc::result({measurements_per_event, spacepoints_per_event});
     }
 
     auto endAlgorithms = std::chrono::system_clock::now();
     std::chrono::duration<double> diffAlgo = endAlgorithms - startAlgorithms;
-    std::cout << "Algorithms time: " << diffAlgo.count() << " sec." << std::endl;
+    std::cout << "Algorithms time: " << diffAlgo.count() << " sec."
+              << std::endl;
 
     std::cout << "==> Statistics ... " << std::endl;
-    std::cout << "- read    " << n_cells << " cells from " << n_modules << " modules" << std::endl;
+    std::cout << "- read    " << n_cells << " cells from " << n_modules
+              << " modules" << std::endl;
     std::cout << "- created " << n_clusters << " clusters. " << std::endl;
-    std::cout << "- created " << n_measurements << " measurements. " << std::endl;
-    std::cout << "- created " << n_space_points << " space points. " << std::endl;
+    std::cout << "- created " << n_measurements << " measurements. "
+              << std::endl;
+    std::cout << "- created " << n_space_points << " space points. "
+              << std::endl;
 
     return aggregated_results;
-
 }
 
 // The main routine
@@ -95,13 +102,15 @@ int main(int argc, char *argv[]) {
 
     if (argc < 4) {
         std::cout << "Not enough arguments, minimum requirement: " << std::endl;
-        std::cout << "./io_dec_par_example <detector_file> <cell_directory> <events>" << std::endl;
+        std::cout
+            << "./io_dec_par_example <detector_file> <cell_directory> <events>"
+            << std::endl;
         return -1;
     }
 
     auto detector_file = std::string(argv[1]);
     auto cell_directory = std::string(argv[2]);
-    auto events = static_cast<size_t> (std::atoi(argv[3]));
+    auto events = static_cast<size_t>(std::atoi(argv[3]));
 
     auto start = std::chrono::system_clock::now();
     vecmem::host_memory_resource resource;
@@ -113,5 +122,6 @@ int main(int argc, char *argv[]) {
 
     auto end = std::chrono::system_clock::now();
     std::chrono::duration<double> diff = end - start;
-    std::cout << "Total execution time: " << diff.count() << " sec." << std::endl;
+    std::cout << "Total execution time: " << diff.count() << " sec."
+              << std::endl;
 }
--- io/include/tmp_edm.hpp
+++ io/include/tmp_edm.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
+#include <map>
+
 #include "edm/cell.hpp"
 #include "edm/measurement.hpp"
 #include "edm/spacepoint.hpp"
 
-#include <map>
-
 namespace traccc {
-    struct result {
-        traccc::host_measurement_container measurements;
-        traccc::host_spacepoint_container spacepoints;
-    };
+struct result {
+    traccc::host_measurement_container measurements;
+    traccc::host_spacepoint_container spacepoints;
+};
 
-    using geometry = std::map<traccc::geometry_id, traccc::transform3>;
-    using demonstrator_input = vecmem::vector<traccc::host_cell_container>;
-    using demonstrator_result = vecmem::vector<traccc::result>;
-}
+using geometry = std::map<traccc::geometry_id, traccc::transform3>;
+using demonstrator_input = vecmem::vector<traccc::host_cell_container>;
+using demonstrator_result = vecmem::vector<traccc::result>;
+}  // namespace traccc
