d953fd6ce788d9d20694323a6e930a5f175f535f
beomki yeo <beomki.yeo@gmail.com>
2021-08-12 16:13:45 -0700

    fix few things

--- core/include/definitions/algebra.hpp
+++ core/include/definitions/algebra.hpp
@@ -7,12 +7,40 @@
 
 #pragma once
 
+// array
 #if defined(ALGEBRA_array)
 #include "plugins/algebra/array_definitions.hpp"
+
+namespace traccc {
+template <typename T, std::size_t N>
+using array = darray<T, N>;
+using transform3 = algebra::array::transform3;
+}  // namespace traccc
+
+// eigen
 #elif defined(ALGEBRA_eigen)
 #include "plugins/algebra/eigen_definitions.hpp"
+
+namespace traccc {
+template <typename T, std::size_t N>
+using array = Eigen::Matrix<T, N, 1>;
+using transform3 = algebra::eigen::transform3;
+}  // namespace traccc
+
+// smatrix
 #elif defined(ALGEBRA_smatrix)
 #include "plugins/algebra/smatrix_definitions.hpp"
+
+namespace traccc {
+using transform3 = algebra::smatrix::transform3;
+}
+
+// vc_array
 #elif defined(ALGEBRA_vc)
 #include "plugins/algebra/vc_array_definitions.hpp"
+
+namespace traccc {
+using transform3 = algebra::vc_array::transform3;
+}
+
 #endif
--- core/include/definitions/primitives.hpp
+++ core/include/definitions/primitives.hpp
@@ -17,15 +17,11 @@ using geometry_id = uint64_t;
 using event_id = uint64_t;
 using channel_id = unsigned int;
 
-template <typename T, std::size_t N>
-using array = darray<T, N>;
 using vector2 = array<scalar, 2>;
 using point2 = array<scalar, 2>;
 using variance2 = array<scalar, 2>;
 using point3 = array<scalar, 3>;
 using vector3 = array<scalar, 3>;
 using variance3 = array<scalar, 3>;
-
-using transform3 = algebra::array::transform3;
 
 }  // namespace traccc
