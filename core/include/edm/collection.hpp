dda115649218178850882ef2bc2ed959fdf1cbd2
beomki yeo <beomki.yeo@gmail.com>
2021-08-14 16:08:27 -0700

    fix comments

--- core/include/edm/collection.hpp
+++ core/include/edm/collection.hpp
@@ -8,25 +8,21 @@
 #pragma once
 
 // VecMem include(s).
-#include <vecmem/containers/data/jagged_vector_buffer.hpp>
 #include <vecmem/containers/data/vector_buffer.hpp>
 #include <vecmem/containers/device_vector.hpp>
-#include <vecmem/containers/jagged_device_vector.hpp>
-#include <vecmem/containers/jagged_vector.hpp>
 #include <vecmem/containers/vector.hpp>
 
 namespace traccc {
 
-/// Container describing objects in a given event
+/// Collection describing objects in a given event
 ///
-/// This is the generic container of the code, holding all relevant
-/// information about objcts in a given event.
+/// This is the generic collection of the code, holding a vector of objects.
 ///
 /// It can be instantiated with different vector types, to be able to use
-/// the same container type in both host and device code.
+/// the same collection type in both host and device code.
 ///
 /// It also can be instantiated with different edm types represented by
-/// header and item type.
+/// item type.
 template <typename item_t, template <typename> class vector_t>
 class collection {
 
@@ -34,7 +30,7 @@ class collection {
     /// @name Type definitions
     /// @{
 
-    /// Vector type used by the container
+    /// Vector type used by the collection
     template <typename T>
     using vector_type = vector_t<T>;
 
@@ -47,35 +43,35 @@ class collection {
     item_vector items;
 };
 
-/// Convenience declaration for the container type to use in host code
+/// Convenience declaration for the collection type to use in host code
 template <typename item_t>
 using host_collection = collection<item_t, vecmem::vector>;
 
-/// Convenience declaration for the container type to use in device code
+/// Convenience declaration for the collection type to use in device code
 template <typename item_t>
 using device_collection = collection<item_t, vecmem::device_vector>;
 
 /// @name Types used to send data back and forth between host and device code
 /// @{
 
-/// Structure holding (some of the) data about the container in host code
+/// Structure holding (some of the) data about the collection in host code
 template <typename item_t>
 struct collection_data {
     vecmem::data::vector_view<item_t> items;
 };
 
-/// Structure holding (all of the) data about the container in host code
+/// Structure holding (all of the) data about the collection in host code
 template <typename item_t>
 struct collection_buffer {
     vecmem::data::vector_buffer<item_t> items;
 };
 
-/// Structure used to send the data about the container to device code
+/// Structure used to send the data about the collection to device code
 ///
 /// This is the type that can be passed to device code as-is. But since in
 /// host code one needs to manage the data describing a
-/// @c traccc::container either using @c traccc::container_data or
-/// @c traccc::container_buffer, it needs to have constructors from
+/// @c traccc::collection either using @c traccc::collection_data or
+/// @c traccc::collection_buffer, it needs to have constructors from
 /// both of those types.
 ///
 /// In fact it needs to be created from one of those types, as such an
@@ -95,10 +91,9 @@ struct collection_view {
     vecmem::data::vector_view<item_t> items;
 };
 
-/// Helper function for making a "simple" object out of the container
+/// Helper function for making a "simple" object out of the collection
 template <typename item_t>
-inline collection_data<item_t> get_data(
-    host_collection<item_t>& cc, vecmem::memory_resource* resource = nullptr) {
+inline collection_data<item_t> get_data(host_collection<item_t>& cc) {
     return {vecmem::get_data(cc.items)};
 }
 
