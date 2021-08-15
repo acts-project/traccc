7acaf32acaa514e1d68d7db38ce5400ccfa0a6ae
beomki yeo <beomki.yeo@gmail.com>
2021-08-15 01:23:22 -0700

    make traccc-algebra-plugins

--- core/include/definitions/algebra.hpp
+++ core/include/definitions/algebra.hpp
@@ -7,10 +7,6 @@
 
 #pragma once
 
-// array
-#if defined(ALGEBRA_ARRAY)
-#include "plugins/algebra/array_definitions.hpp"
-
 namespace traccc {
 namespace array {
 
@@ -19,42 +15,15 @@ using array = darray<T, N>;
 using transform3 = algebra::array::transform3;
 
 }  // namespace array
-}  // namespace traccc
 
-#endif
-
-// eigen
-#if defined(ALGEBRA_EIGEN)
-#include "plugins/algebra/eigen_definitions.hpp"
-
-namespace traccc {
 namespace eigen {
 
 template <typename T, std::size_t N>
 using array = Eigen::Matrix<T, N, 1>;
 using transform3 = algebra::eigen::transform3;
-
+    
 }  // namespace eigen
+    
 }  // namespace traccc
 
-#endif
 
-// smatrix
-#if defined(ALGEBRA_SMATRIX)
-#include "plugins/algebra/smatrix_definitions.hpp"
-
-namespace traccc {
-namespace smatrix {}  // namespace smatrix
-}  // namespace traccc
-
-#endif
-
-// vc_array
-#if defined(ALGEBRA_VC)
-#include "plugins/algebra/vc_array_definitions.hpp"
-
-namespace traccc {
-namespace vc {}  // namespace vc
-}  // namespace traccc
-
-#endif
