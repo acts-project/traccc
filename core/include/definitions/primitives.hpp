898a88c5bb9af685f467e397fbe7cb37f5108e07
beomki-yeo <beomki.yeo@gmail.com>
2021-08-30 12:03:30 -0700

    use __plugin

--- core/include/definitions/primitives.hpp
+++ core/include/definitions/primitives.hpp
@@ -9,7 +9,17 @@
 
 #include <stdint.h>
 
-#include "definitions/algebra.hpp"
+#ifdef ALGEBRA_PLUGIN_INCLUDE_ARRAY
+#include "plugins/algebra/array_definitions.hpp"
+#elif ALGEBRA_PLUGIN_INCLUDE_EIGEN
+#include "plugins/algebra/eigen_definitions.hpp"
+#elif ALGEBRA_PLUGIN_INCLUDE_SMATRIX
+#include "plugins/algebra/smatrix_definitions.hpp"
+#elif ALGEBRA_PLUGIN_INCLUDE_VC
+#include "plugins/algebra/vc_definitions.hpp"
+#elif ALGEBRA_PLUGIN_INCLUDE_VECMEM
+#include "plugins/algebra/vecmem_definitions.hpp"
+#endif
 
 namespace traccc {
 
@@ -17,11 +27,12 @@ using geometry_id = uint64_t;
 using event_id = uint64_t;
 using channel_id = unsigned int;
 
-using vector2 = array<scalar, 2>;
-using point2 = array<scalar, 2>;
-using variance2 = array<scalar, 2>;
-using point3 = array<scalar, 3>;
-using vector3 = array<scalar, 3>;
-using variance3 = array<scalar, 3>;
+using vector2 = __plugin::point2;
+using point2 = __plugin::point2;
+using variance2 = __plugin::point2;
+using point3 = __plugin::point3;
+using vector3 = __plugin::point3;
+using variance3 = __plugin::point3;
+using transform3 = __plugin::transform3;
 
 }  // namespace traccc
