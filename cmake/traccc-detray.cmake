f6dc947bb025a597acebb64dd2c0402547933713
beomki-yeo <beomki.yeo@gmail.com>
2021-09-13 20:52:18 -0700

    dependency setup

---
+++ cmake/traccc-detray.cmake
@@ -0,0 +1,42 @@
+# TRACCC library, part of the ACTS project (R&D line)
+#
+# (c) 2021 CERN for the benefit of the ACTS project
+#
+# Mozilla Public License Version 2.0
+
+# Guard against multiple includes.
+include_guard( GLOBAL )
+
+# Look for Detray, quietly first time around. Since if it is not found (which is
+# likely), it prints multiple lines of warnings.
+find_package( detray QUIET )
+
+# If it was found, then we're finished.
+if( detray_FOUND )
+   # Call find_package again, just to nicely print where it is picked up from.
+   find_package( detray )
+   return()
+endif()
+
+# Tell the user what's happening.
+message( STATUS "Building detray as part of the traccc project" )
+
+# Declare where to get VecMem from.
+FetchContent_Declare( Detray
+  GIT_REPOSITORY "https://github.com/acts-project/detray.git"
+  GIT_TAG        552d5622a0dc4aeb99a13576b4ebc12ca469eadd)
+
+# Prevent Detray from building its tests and benchmarks
+# builds/uses GoogleTest.
+set( DETRAY_UNIT_TESTS OFF )
+set( DETRAY_BENCHMARKS OFF )
+set( DETRAY_CUSTOM_SCALARTYPE ${TRACCC_CUSTOM_SCALARTYPE} )
+set( DETRAY_${TRACCC_ALGEBRA_PLUGIN}_PLUGIN ON )
+if(TRACC_BUILD_CUDA)
+  set( DETRAY_BUILD_CUDA ON )
+endif()  
+
+# Get it into the current directory.
+FetchContent_Populate( Detray )
+add_subdirectory( "${detray_SOURCE_DIR}" "${detray_BINARY_DIR}"
+  EXCLUDE_FROM_ALL )
