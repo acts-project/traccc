3daa0de5d82a7537b2108beabb4dd92c39276bff
Konrad Kusiak <kkusiak@atspot01.cern.ch>
2022-01-31 13:32:00 +0100

    Applied the requested changes

--- examples/algorithms/sycl/include/sycl/track_finding/seeding_algorithm.hpp
+++ examples/algorithms/sycl/include/sycl/track_finding/seeding_algorithm.hpp
@@ -52,7 +52,7 @@ class seeding_algorithm
 
     output_type operator()(
         host_spacepoint_container&& spacepoints) const override {
-        output_type seeds({host_seed_container(1, &m_mr.get())});
+        output_type seeds(1, &m_mr.get());
         auto internal_sp_g2 = sb->operator()(std::move(spacepoints));
         seeds =
             sf->operator()(std::move(spacepoints), std::move(internal_sp_g2));
