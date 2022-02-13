7b84b327c08f70c2f4df5f80769e7183ddf91282
beomki-yeo <beomki.yeo@gmail.com>
2022-02-12 22:53:45 -0800

    remove simulation data check

--- tests/io/test_mapper.cpp
+++ tests/io/test_mapper.cpp
@@ -17,142 +17,9 @@
 std::size_t event = 0;
 
 std::string detector_file = "/tml_detector/trackml-detector.csv";
-std::string hits_dir = "tml_pixels/ttbar_200/";
-std::string cells_dir = "tml_pixels/ttbar_200/";
-std::string particles_dir = "tml_pixels/ttbar_200/";
-
-std::string mock_hits_dir = "../tests/io/mock_data/";
-std::string mock_cells_dir = "../tests/io/mock_data/";
-std::string mock_particles_dir = "../tests/io/mock_data/";
-
-/***
- * Simulation data test
- */
-
-// Test generate_particle_map function
-TEST(mappper, particle_map) {
-    auto p_map = traccc::generate_particle_map(event, particles_dir);
-
-    for (auto const& [pid, ptc] : p_map) {
-        if (pid == 4503600147464192) {
-            EXPECT_EQ(ptc.pos, traccc::vector3({-0.0120002991, -9.48547313e-05,
-                                                -15.1165705}));
-        }
-
-        else if (pid == 522486832617750528) {
-            EXPECT_EQ(ptc.pos,
-                      traccc::vector3({-16.0845699, -43.6837349, 33313.7969}));
-        }
-
-        else if (pid == 815244996511793152) {
-            EXPECT_EQ(ptc.pos, traccc::vector3(
-                                   {0.00947579276, 0.00682933675, 59.2630196}));
-        }
-    }
-}
-
-// Test generate_hit_particle_map function
-TEST(mappper, hit_particle_map) {
-    auto h_p_map =
-        traccc::generate_hit_particle_map(event, hits_dir, particles_dir);
-
-    for (auto const& [hit, ptc] : h_p_map) {
-
-        if (hit.global == traccc::point3{39.2037048, 0.352969825, -1502.5}) {
-            EXPECT_EQ(ptc.particle_id, 58546796263112704);
-        }
-
-        else if (hit.global == traccc::point3{65.2966003, -89.9105911, 598}) {
-            EXPECT_EQ(ptc.particle_id, 774619137115684864);
-        }
-
-        else if (hit.global ==
-                 traccc::point3{-631.891235, 3.07215524, 1215.5}) {
-            EXPECT_EQ(ptc.particle_id, 369295170719449088);
-        }
-    }
-}
-
-// Test generate_hit_map function
-TEST(mappper, hit_map) {
-    auto h_map = traccc::generate_hit_map(event, hits_dir);
-
-    for (auto const& [hid, hit] : h_map) {
-        if (hid == 0) {
-            EXPECT_EQ(hit.global,
-                      traccc::point3({39.2037048, 0.352969825, -1502.5}));
-        }
-
-        else if (hid == 74290) {
-            EXPECT_EQ(hit.global,
-                      traccc::point3({-269.925323, 419.792511, -480.266479}));
-        }
-
-        else if (hid == 92349) {
-            EXPECT_EQ(hit.global,
-                      traccc::point3({-878.646667, -10.9199247, 2952.5}));
-        }
-    }
-}
-
-// Test generate_hit_cell_map function
-TEST(mappper, hit_cell_map) {
-    auto h_c_map = traccc::generate_hit_cell_map(event, cells_dir, hits_dir);
-
-    auto compare_cells = [](std::vector<traccc::cell>& cells1,
-                            std::vector<traccc::cell>& cells2) {
-        std::sort(cells1.begin(), cells1.end());
-        std::sort(cells2.begin(), cells2.end());
-
-        EXPECT_EQ(cells1, cells2);
-    };
-
-    for (auto [hit, cells] : h_c_map) {
-        if (hit.global == traccc::point3{-135.178787, -101.632271, -958}) {
-            std::vector<traccc::cell> _cells;
-            _cells.push_back({331, 141, 0.025897909, 0});
-
-            compare_cells(cells, _cells);
-        }
-
-        else if (hit.global == traccc::point3{-64.083931, -47.5486031, 822.5}) {
-            std::vector<traccc::cell> _cells;
-            _cells.push_back({277, 992, 0.0143668521, 0});
-
-            compare_cells(cells, _cells);
-        }
-
-        else if (hit.global == traccc::point3{823.129333, 517.057373, 1222.5}) {
-            std::vector<traccc::cell> _cells;
-            _cells.push_back({636, 10, 0.0697815791, 0});
-            _cells.push_back({637, 10, 0.186917365, 0});
-            _cells.push_back({638, 10, 0.0348203704, 0});
-
-            compare_cells(cells, _cells);
-        }
-    }
-}
-
-// Test generate_cell_particle_map function
-TEST(mappper, cell_particle_map) {
-    auto c_p_map = traccc::generate_cell_particle_map(event, cells_dir,
-                                                      hits_dir, particles_dir);
-
-    for (auto const& [c, ptc] : c_p_map) {
-
-        if (c == traccc::cell{222, 1257, 0.0041470062, 0}) {
-            EXPECT_EQ(ptc.particle_id, 58546796263112704);
-        }
-
-        else if (c == traccc::cell{114, 1403, 0.00306466641, 0}) {
-            EXPECT_EQ(ptc.particle_id, 99079195543470080);
-        }
-
-        else if (c == traccc::cell{232, 1205, 0.00790336262, 0}) {
-            EXPECT_EQ(ptc.particle_id, 702628615719813121);
-        }
-    }
-}
+std::string hits_dir = "../tests/io/mock_data/";
+std::string cells_dir = "../tests/io/mock_data/";
+std::string particles_dir = "../tests/io/mock_data/";
 
 /***
  * Mock data test
@@ -180,8 +47,8 @@ TEST(mappper, cell_particle_map) {
  */
 
 // Test generate_particle_map function
-TEST(mappper, mock_particle_map) {
-    auto p_map = traccc::generate_particle_map(event, mock_particles_dir);
+TEST(mappper, particle_map) {
+    auto p_map = traccc::generate_particle_map(event, particles_dir);
 
     EXPECT_EQ(p_map.size(), 3);
 
@@ -198,9 +65,9 @@ TEST(mappper, mock_particle_map) {
 }
 
 // Test generate_hit_particle_map function
-TEST(mappper, mock_hit_particle_map) {
-    auto h_p_map = traccc::generate_hit_particle_map(event, mock_hits_dir,
-                                                     mock_particles_dir);
+TEST(mappper, hit_particle_map) {
+    auto h_p_map =
+        traccc::generate_hit_particle_map(event, hits_dir, particles_dir);
 
     EXPECT_EQ(h_p_map.size(), 3);
 
@@ -219,8 +86,8 @@ TEST(mappper, mock_hit_particle_map) {
 }
 
 // Test generate_hit_map function
-TEST(mappper, mock_hit_map) {
-    auto h_map = traccc::generate_hit_map(event, mock_hits_dir);
+TEST(mappper, hit_map) {
+    auto h_map = traccc::generate_hit_map(event, hits_dir);
 
     EXPECT_EQ(h_map.size(), 3);
 
@@ -230,9 +97,8 @@ TEST(mappper, mock_hit_map) {
 }
 
 // Test generate_hit_cell_map function
-TEST(mappper, mock_hit_cell_map) {
-    auto h_c_map =
-        traccc::generate_hit_cell_map(event, mock_cells_dir, mock_hits_dir);
+TEST(mappper, hit_cell_map) {
+    auto h_c_map = traccc::generate_hit_cell_map(event, cells_dir, hits_dir);
 
     EXPECT_EQ(h_c_map.size(), 3);
 
@@ -267,10 +133,10 @@ TEST(mappper, mock_hit_cell_map) {
 }
 
 // Test generate_cell_particle_map function
-TEST(mappper, mock_cell_particle_map) {
+TEST(mappper, cell_particle_map) {
 
-    auto c_p_map = traccc::generate_cell_particle_map(
-        event, mock_cells_dir, mock_hits_dir, mock_particles_dir);
+    auto c_p_map = traccc::generate_cell_particle_map(event, cells_dir,
+                                                      hits_dir, particles_dir);
 
     std::vector<traccc::cell> cells;
     traccc::cell cell0{1, 0, 0.0041470062, 0};
@@ -297,7 +163,7 @@ TEST(mappper, mock_cell_particle_map) {
 }
 
 // Test generate_measurement_cell_map function
-TEST(mappper, mock_measurement_cell_map) {
+TEST(mappper, measurement_cell_map) {
 
     auto compare_cells = [](std::vector<traccc::cell>& cells1,
                             std::vector<traccc::cell>& cells2) {
@@ -309,8 +175,8 @@ TEST(mappper, mock_measurement_cell_map) {
 
     vecmem::host_memory_resource resource;
 
-    auto m_c_map = traccc::generate_measurement_cell_map(
-        event, detector_file, mock_cells_dir, resource);
+    auto m_c_map = traccc::generate_measurement_cell_map(event, detector_file,
+                                                         cells_dir, resource);
 
     std::vector<traccc::cell> cells0;
     cells0.push_back({1, 0, 0.0041470062, 0});
@@ -351,13 +217,12 @@ TEST(mappper, mock_measurement_cell_map) {
 }
 
 // Test the first generate_measurement_particle_map function
-TEST(mappper, mock_measurement_particle_map_with_clusterization) {
+TEST(mappper, measurement_particle_map_with_clusterization) {
 
     vecmem::host_memory_resource mr;
 
     auto m_p_map = traccc::generate_measurement_particle_map(
-        event, detector_file, mock_cells_dir, mock_hits_dir, mock_particles_dir,
-        mr);
+        event, detector_file, cells_dir, hits_dir, particles_dir, mr);
 
     // There are two measurements (or clusters)
     EXPECT_EQ(m_p_map.size(), 2);
@@ -411,12 +276,12 @@ TEST(mappper, mock_measurement_particle_map_with_clusterization) {
 }
 
 // Test the second generate_measurement_particle_map function
-TEST(mappper, mock_measurement_particle_map_without_clusterization) {
+TEST(mappper, measurement_particle_map_without_clusterization) {
 
     vecmem::host_memory_resource mr;
 
     auto m_p_map = traccc::generate_measurement_particle_map(
-        event, detector_file, mock_hits_dir, mock_particles_dir, mr);
+        event, detector_file, hits_dir, particles_dir, mr);
 
     // Without clusterization, there are three measurements each of which is
     // from each particle
