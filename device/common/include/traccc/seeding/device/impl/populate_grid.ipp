0dc38d85e46f00872a93c1e094a303391d764901
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2022-04-08 19:03:54 +0200

    Shared the spacepoint binning code between CUDA and SYCL.
    Moved the CUDA algorithms into functions that could be used
    by both the CUDA and SYCL code, avoiding code duplication.

---
+++ device/common/include/traccc/seeding/device/impl/populate_grid.ipp
@@ -0,0 +1,55 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021-2022 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Project include(s).
+#include "traccc/seeding/spacepoint_binning_helper.hpp"
+
+namespace traccc::device {
+
+TRACCC_DEVICE
+void populate_grid(
+    std::size_t globalIndex, const seedfinder_config& config,
+    const spacepoint_container_const_view& spacepoints_view,
+    const vecmem::data::vector_view<const get_spacepoint_prefix_sum_element_t>&
+        sp_prefix_sum_view,
+    sp_grid_view grid_view) {
+
+    // Check if anything needs to be done.
+    vecmem::device_vector<const get_spacepoint_prefix_sum_element_t>
+        sp_prefix_sum(sp_prefix_sum_view);
+    if (globalIndex >= sp_prefix_sum.size()) {
+        return;
+    }
+
+    // Get the spacepoint that we need to look at.
+    const get_spacepoint_prefix_sum_element_t sp_idx =
+        sp_prefix_sum[globalIndex];
+    device_spacepoint_const_container spacepoints(spacepoints_view);
+    const spacepoint sp = spacepoints.at(sp_idx);
+
+    /// Check out if the spacepoint can be used for seeding.
+    if (is_valid_sp(config, sp) != detray::invalid_value<size_t>()) {
+
+        // Set up the spacepoint grid object(s).
+        sp_grid_device grid(grid_view);
+        const sp_grid_device::axis_p0_type& phi_axis = grid.axis_p0();
+        const sp_grid_device::axis_p1_type& z_axis = grid.axis_p1();
+
+        // Find the grid bin that the spacepoint belongs to.
+        const internal_spacepoint<spacepoint> isp(spacepoints, sp_idx,
+                                                  config.beamPos);
+        const std::size_t bin_index =
+            phi_axis.bin(isp.phi()) + phi_axis.bins() * z_axis.bin(isp.z());
+
+        // Add the spacepoint to the grid.
+        grid.bin(bin_index).push_back(std::move(isp));
+    }
+}
+
+}  // namespace traccc::device
--- device/cuda/include/traccc/cuda/seeding/spacepoint_binning.hpp
+++ device/cuda/include/traccc/cuda/seeding/spacepoint_binning.hpp
@@ -8,88 +8,39 @@
 #pragma once
 
 // Project include(s).
-#include "traccc/cuda/seeding/counting_grid_capacities.hpp"
-#include "traccc/cuda/seeding/populating_grid.hpp"
-#include "traccc/seeding/spacepoint_binning_helper.hpp"
+#include "traccc/edm/spacepoint.hpp"
+#include "traccc/seeding/detail/seeding_config.hpp"
+#include "traccc/seeding/detail/spacepoint_grid.hpp"
 #include "traccc/utils/algorithm.hpp"
 
 // VecMem include(s).
-#include <vecmem/memory/cuda/device_memory_resource.hpp>
+#include <vecmem/memory/memory_resource.hpp>
 
-// Thrust include(s).
-//#include <thrust/sort.h>
+// System include(s).
+#include <functional>
+#include <utility>
 
-namespace traccc {
-namespace cuda {
+namespace traccc::cuda {
 
-/// Spacepoing binning for cuda
-struct spacepoint_binning
-    : public algorithm<sp_grid_buffer(host_spacepoint_container&&)> {
+/// Spacepoing binning executed on a CUDA device
+class spacepoint_binning
+    : public algorithm<sp_grid_buffer(const host_spacepoint_container&)> {
 
+    public:
+    /// Constructor for the algorithm
     spacepoint_binning(const seedfinder_config& config,
                        const spacepoint_grid_config& grid_config,
-                       vecmem::memory_resource& mr)
-        : m_config(config), m_grid_config(grid_config), m_mr(mr) {
-        m_axes = get_axes(grid_config, mr);
-    }
+                       vecmem::memory_resource& mr);
 
-    output_type operator()(
-        host_spacepoint_container&& spacepoints) const override {
+    /// Function executing the algorithm
+    sp_grid_buffer operator()(
+        const host_spacepoint_container& spacepoints) const override;
 
-        int nbins = m_axes.first.n_bins * m_axes.second.n_bins;
-
-        // Store the container id for spacepoints
-        vecmem::vector<std::pair<unsigned int, unsigned int>>
-            sp_container_indices(spacepoints.total_size(), &m_mr.get());
-
-        std::size_t k = 0;
-        for (std::size_t i = 0; i < spacepoints.size(); i++) {
-            std::size_t n_items = spacepoints.get_items()[i].size();
-
-            for (std::size_t j = 0; j < n_items; j++) {
-                sp_container_indices[k++] = std::make_pair(i, j);
-            }
-        }
-
-        // Capacity for the bins of grid buffer
-        vecmem::vector<unsigned int> grid_capacities(nbins, 0, &m_mr.get());
-
-        // Run counting grid capacities
-        traccc::cuda::counting_grid_capacities(
-            m_config, m_axes.first, m_axes.second, spacepoints,
-            sp_container_indices, grid_capacities, m_mr.get());
-
-        // Create size and capacity vector for grid buffer
-        std::vector<std::size_t> sizes(nbins, 0);
-        std::vector<std::size_t> capacities;
-        capacities.reserve(nbins);
-        for (const auto& c : grid_capacities) {
-            /// Note: Need to investigate why populating_grid fails without this
-            /// when the data size is small.
-            if (c < 60) {
-                capacities.push_back(60);
-                continue;
-            }
-            capacities.push_back(c);
-        }
-
-        // Create grid buffer
-        output_type g2_buffer(m_axes.first, m_axes.second, sizes, capacities,
-                              m_mr.get());
-
-        // Run populating grid
-        traccc::cuda::populating_grid(m_config, g2_buffer, spacepoints,
-                                      sp_container_indices, grid_capacities,
-                                      m_mr.get());
-
-        return g2_buffer;
-    }
-
+    private:
     seedfinder_config m_config;
-    spacepoint_grid_config m_grid_config;
-    std::pair<output_type::axis_p0_type, output_type::axis_p1_type> m_axes;
+    std::pair<sp_grid::axis_p0_type, sp_grid::axis_p1_type> m_axes;
     std::reference_wrapper<vecmem::memory_resource> m_mr;
-};
 
-}  // namespace cuda
-}  // namespace traccc
+};  // class spacepoint_binning
+
+}  // namespace traccc::cuda
