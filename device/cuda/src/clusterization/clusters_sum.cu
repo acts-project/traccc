86abcbae292503e03a2584974d59f72046349c9b
Chamodya_ka <chamodya@ackcio.com>
2022-06-14 12:54:08 +0530

    lisence added

--- device/cuda/src/clusterization/clusters_sum.cu
+++ device/cuda/src/clusterization/clusters_sum.cu
@@ -1,4 +1,9 @@
-
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
 
 #include "clusters_sum.hpp"
 #include "traccc/cuda/utils/definitions.hpp"
