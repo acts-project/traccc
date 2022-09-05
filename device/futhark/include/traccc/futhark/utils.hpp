1c371660c778659f123710c81276d4d017369353
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2022-09-05 11:44:28 +0200

    Add Futhark implementation of CCA
    This follows the existing FastSV algorithm.

--- device/futhark/include/traccc/futhark/utils.hpp
+++ device/futhark/include/traccc/futhark/utils.hpp
@@ -6,14 +6,17 @@
  */
 
 #pragma once
-
 #include <sstream>
 #include <stdexcept>
+#include <string>
+#include <traccc/futhark/context.hpp>
 
-#define FUTHARK_ERROR_CHECK(ans) futharkAssert((ans), __FILE__, __LINE__);
+#define FUTHARK_ERROR_CHECK(ans) futharkAssert((ans), __FILE__, __LINE__)
 
 inline void futharkAssert(int code, const char *file, int line,
                           bool abort = true) {
+    char *error = futhark_context_get_error(&traccc::futhark::get_context());
+
     if (code == 2) {
         throw std::runtime_error(
             "Futhark program exited due to a programming error.");
@@ -24,6 +27,10 @@ inline void futharkAssert(int code, const char *file, int line,
         std::stringstream ss;
         ss << "Futhark program exited with unknown non-zero return code "
            << code << ".";
+
+        if (error != nullptr) {
+            ss << " Additional error informtion: " << std::string(error);
+        }
 
         throw std::runtime_error(ss.str());
     }
