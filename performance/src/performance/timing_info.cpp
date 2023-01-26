2b8711e4f71a59380e02edaa20ff839f50b837b0
xola <xola.mapekula@cern.ch>
2023-01-26 18:05:14 +0200

    fixing issue with kokkos, 'declaration of 'it' shadows a previous local', changed it -> iter inside find_if

--- performance/src/performance/timing_info.cpp
+++ performance/src/performance/timing_info.cpp
@@ -21,7 +21,7 @@ std::chrono::nanoseconds timing_info::get_time(
 
     auto it = std::find_if(
         data.begin(), data.end(),
-        [&timer_name](timing_info_pair it) { return it.first == timer_name; });
+        [&timer_name](timing_info_pair iter) { return iter.first == timer_name; });
     if (it == data.end()) {
         throw std::invalid_argument("Unknown component name received");
     }
