fe07973bc46e903d3bfa09ae2b22d9a623a35fe3
xola <xola.mapekula@cern.ch>
2023-02-02 15:34:29 +0200

    nvtx integration with timer class

--- performance/src/performance/timer.cpp
+++ performance/src/performance/timer.cpp
@@ -11,6 +11,11 @@
 // System include(s).
 #include <algorithm>
 
+// Nividia tool extensions library
+#ifdef TRACCC_HAVE_NVTX
+#include "nvtx3/nvToolsExt.h"
+#endif  // TRACCC_HAVE_NVTX
+	
 namespace traccc::performance {
 
 /// Start time measurement
@@ -19,10 +24,18 @@ namespace traccc::performance {
 timer::timer(const std::string_view timer_name, timing_info& t_info)
     : m_start(std::chrono::high_resolution_clock::now()),
       m_name(timer_name),
-      m_timing_info(t_info) {}
+      m_timing_info(t_info) {
+      #ifdef TRACCC_HAVE_NVTX
+       nvtxRangePushA(timer_name.data());
+      #endif  // TRACCC_HAVE_NVTX
+      
+      }
 
 /// End time measurement
 timer::~timer() {
+    #ifdef TRACCC_HAVE_NVTX
+    nvtxRangePop();
+    #endif  // TRACCC_HAVE_NVTX	
     const auto end = std::chrono::high_resolution_clock::now();
     const std::chrono::nanoseconds totalTime = end - m_start;
     const auto pos =
