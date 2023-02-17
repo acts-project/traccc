1014cc1fe7c936e3bb67cf467b2adcb7b249ac47
Ryan Cross <ryan.cross@warwick.ac.uk>
2023-02-17 09:42:51 +0000

    Add basics of device code.

---
+++ device/alpaka/include/traccc/alpaka/utils/definitions.hpp
@@ -0,0 +1,19 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+#include <alpaka/alpaka.hpp>
+#include <alpaka/example/ExampleDefaultAcc.hpp>
+
+namespace traccc::alpaka {
+
+using Dim = ::alpaka::DimInt<1>;
+using Idx = uint32_t;
+using WorkDiv = ::alpaka::WorkDivMembers<Dim, Idx>;
+
+}  // namespace traccc::alpaka
---
+++ device/alpaka/src/seeding/spacepoint_binning.cpp
@@ -0,0 +1,176 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021-2022 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Local include(s).
+#include "traccc/alpaka/seeding/spacepoint_binning.hpp"
+
+#include "traccc/alpaka/utils/definitions.hpp"
+
+// Project include(s).
+#include "traccc/edm/spacepoint.hpp"
+#include "traccc/seeding/device/count_grid_capacities.hpp"
+#include "traccc/seeding/device/populate_grid.hpp"
+
+// VecMem include(s).
+#include <vecmem/utils/copy.hpp>
+
+namespace traccc::alpaka {
+
+spacepoint_binning::spacepoint_binning(
+    const seedfinder_config& config, const spacepoint_grid_config& grid_config,
+    const traccc::memory_resource& mr)
+    : m_config(config.toInternalUnits()),
+      m_axes(get_axes(grid_config.toInternalUnits(), *(mr.host))),
+      m_mr(mr) {
+    m_copy = std::make_unique<vecmem::copy>();
+}
+
+// Grid Capacity Kernel
+struct CountGridCapacityKernel {
+    template <typename Acc>
+    ALPAKA_FN_ACC void operator()(
+        Acc const& acc,
+        const seedfinder_config& config,
+        const sp_grid::axis_p0_type& phi_axis,
+        const sp_grid::axis_p1_type& z_axis,
+        const spacepoint_collection_types::const_view& spacepoints_view,
+        vecmem::data::vector_view<unsigned int>& grid_capacities_view
+    ) const
+    {
+        auto const globalThreadIdx = ::alpaka::getIdx<::alpaka::Grid, ::alpaka::Threads>(acc)[0u];
+        device::count_grid_capacities(globalThreadIdx, config,
+                                      phi_axis, z_axis, spacepoints_view,
+                                      grid_capacities_view);
+    }
+};
+
+// Populate Grid Kernel
+struct PopulateGridKernel {
+    template <typename Acc>
+    ALPAKA_FN_ACC void operator()(
+        Acc const& acc,
+        const seedfinder_config& config,
+        const spacepoint_collection_types::const_view& spacepoints_view,
+        sp_grid_view& grid_view
+    ) const
+    {
+        auto const globalThreadIdx = ::alpaka::getIdx<::alpaka::Grid, ::alpaka::Threads>(acc)[0u];
+
+        // Check if anything needs to be done.
+        const spacepoint_collection_types::const_device spacepoints(
+            spacepoints_view);
+
+        if (globalThreadIdx >= spacepoints.size()) {
+            return;
+        }
+        const spacepoint sp = spacepoints.at(globalThreadIdx);
+
+        /// Check out if the spacepoint can be used for seeding.
+        if (is_valid_sp(config, sp) != detray::detail::invalid_value<size_t>()) {
+
+            // Set up the spacepoint grid object(s).
+            sp_grid_device grid(grid_view);
+            const sp_grid_device::axis_p0_type& phi_axis = grid.axis_p0();
+            const sp_grid_device::axis_p1_type& z_axis = grid.axis_p1();
+
+            // Find the grid bin that the spacepoint belongs to.
+            const internal_spacepoint<spacepoint> isp(sp, globalThreadIdx,
+                                                      config.beamPos);
+            const std::size_t bin_index =
+                phi_axis.bin(isp.phi()) + phi_axis.bins() * z_axis.bin(isp.z());
+
+            // Add the spacepoint to the grid.
+            grid.bin(bin_index).push_back(std::move(isp));
+        }
+    }
+};
+
+
+spacepoint_binning::output_type spacepoint_binning::operator()(
+    const spacepoint_collection_types::const_view& spacepoints_view) const {
+
+    // Setup alpaka
+    using Acc = ::alpaka::ExampleDefaultAcc<Dim, Idx>;
+    using Queue = ::alpaka::Queue<Acc, ::alpaka::Blocking>;
+    std::cout << "Using alpaka accelerator: " << ::alpaka::getAccName<Acc>() << std::endl;
+    auto devAcc = ::alpaka::getDevByIdx<Acc>(0u);
+    auto queue = Queue{devAcc};
+
+    // Get the spacepoint sizes from the view
+    auto sp_size = m_copy->get_size(spacepoints_view);
+    const uint32_t n(spacepoints_view.size());
+
+    // Set up the container that will be filled with the required capacities for
+    // the spacepoint grid.
+    const std::size_t grid_bins = m_axes.first.n_bins * m_axes.second.n_bins;
+    vecmem::data::vector_buffer<unsigned int> grid_capacities_buff(grid_bins,
+                                                                   m_mr.main);
+    m_copy->setup(grid_capacities_buff);
+    m_copy->memset(grid_capacities_buff, 0);
+    vecmem::data::vector_view<unsigned int> grid_capacities_view =
+        grid_capacities_buff;
+
+    // Now define the Alpaka Work division
+    using WorkDiv = ::alpaka::WorkDivMembers<::alpaka::DimInt<1u>, uint32_t>;
+    auto const deviceProperties = ::alpaka::getAccDevProps<Acc>(devAcc);
+    auto const maxThreadsPerBlock = deviceProperties.m_blockThreadExtentMax[0];
+    auto const threadsPerBlock = maxThreadsPerBlock;
+    auto const blocksPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+    auto const elementsPerThread = 1u;
+    auto workDiv = WorkDiv{blocksPerGrid, threadsPerBlock, elementsPerThread};
+    auto bufAcc = ::alpaka::allocBuf<float, uint32_t>(devAcc, n);
+
+    // Kokkos::parallel_for(
+    //     "count_grid_capacities", team_policy(num_blocks, num_threads),
+    //     KOKKOS_LAMBDA(const member_type& team_member) {
+    //         device::count_grid_capacities(
+    //             team_member.league_rank() * team_member.team_size() +
+    //                 team_member.team_rank(),
+    //             m_config, m_axes.first, m_axes.second, spacepoints_view,
+    //             grid_capacities_view);
+    //     });
+    ::alpaka::exec<Acc>(
+            queue, workDiv,
+            CountGridCapacityKernel{},
+            m_config, m_axes.first, m_axes.second,
+            spacepoints_view, grid_capacities_view
+    );
+
+    // Copy grid capacities back to the host
+    vecmem::vector<unsigned int> grid_capacities_host(m_mr.host ? m_mr.host
+                                                                : &(m_mr.main));
+    (*m_copy)(grid_capacities_buff, grid_capacities_host);
+
+    // Create the grid buffer.
+    sp_grid_buffer grid_buffer(
+        m_axes.first, m_axes.second, std::vector<std::size_t>(grid_bins, 0),
+        std::vector<std::size_t>(grid_capacities_host.begin(),
+                                 grid_capacities_host.end()),
+        m_mr.main, m_mr.host);
+    m_copy->setup(grid_buffer._buffer);
+    sp_grid_view grid_view = grid_buffer;
+
+    // Populate the grid.
+    // Kokkos::parallel_for(
+    //     "populate_grid", team_policy(num_blocks, num_threads),
+    //     KOKKOS_LAMBDA(const member_type& team_member) {
+    //         device::populate_grid(
+    //             team_member.league_rank() * team_member.team_size() +
+    //                 team_member.team_rank(),
+    //             m_config, spacepoints_view, grid_view);
+    //     });
+    ::alpaka::exec<Acc>(
+            queue, workDiv,
+            PopulateGridKernel{},
+            m_config, spacepoints_view, grid_view
+    );
+
+    // Return the freshly filled buffer.
+    return grid_buffer;
+}
+
+}  // namespace traccc::alpaka
