086053ad757418f952b0f9f9d5d6f43a35557d3e
xola <xola.mapekula@cern.ch>
2023-04-18 16:42:40 +0200

    clang format files

--- examples/run/cuda/inst_nvtx.cpp
+++ examples/run/cuda/inst_nvtx.cpp
@@ -1,5 +1,5 @@
 /* Copyright (c) 2014, NVIDIA CORPORATION. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -25,70 +25,64 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdio.h>
-#include <dlfcn.h>
 #include <cxxabi.h>
+#include <dlfcn.h>
+#include <stdio.h>
+
 #include "nvtx3/nvToolsExt.h"
 #define UNUSED(x) (void)(x)
-const char* const default_name = "Unknown";
+const char *const default_name = "Unknown";
 
-const uint32_t colors[] = { 0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff0000, 0xffffffff };
-const int num_colors = sizeof(colors)/sizeof(uint32_t);
+const uint32_t colors[] = {0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff,
+                           0xff00ffff, 0xffff0000, 0xffffffff};
+const int num_colors = sizeof(colors) / sizeof(uint32_t);
 static int color_id = 0;
 
-extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site) __attribute__((no_instrument_function));
-extern "C" void __cyg_profile_func_exit(void *this_fn, void *call_site) __attribute__((no_instrument_function));
+extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site)
+    __attribute__((no_instrument_function));
+extern "C" void __cyg_profile_func_exit(void *this_fn, void *call_site)
+    __attribute__((no_instrument_function));
 
-void rangePush(const char* const name ) __attribute__((no_instrument_function));
+void rangePush(const char *const name) __attribute__((no_instrument_function));
 void rangePop() __attribute__((no_instrument_function));
 
-extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site)
-{
+extern "C" void __cyg_profile_func_enter(void *this_fn, void *call_site) {
     (void)this_fn;
     (void)call_site;
-	Dl_info this_fn_info;
-	if ( dladdr( this_fn, &this_fn_info ) )
-	{
-		int status = 0;
-		rangePush(abi::__cxa_demangle(this_fn_info.dli_sname,0, 0, &status));
-	}
-	else
-	{
-		rangePush(default_name);
-	}
+    Dl_info this_fn_info;
+    if (dladdr(this_fn, &this_fn_info)) {
+        int status = 0;
+        rangePush(abi::__cxa_demangle(this_fn_info.dli_sname, 0, 0, &status));
+    } else {
+        rangePush(default_name);
+    }
 } /* __cyg_profile_func_enter */
 
-extern "C" void __cyg_profile_func_exit(void *this_fn, void *call_site)
-{
-        (void)this_fn;
-        (void)call_site;
-	rangePop();
+extern "C" void __cyg_profile_func_exit(void *this_fn, void *call_site) {
+    (void)this_fn;
+    (void)call_site;
+    rangePop();
 } /* __cyg_profile_func_enter */
 
-void rangePush(const char* const name )
-{
-	nvtxEventAttributes_t eventAttrib ;
-	eventAttrib.version = NVTX_VERSION;
-	eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
-	eventAttrib.colorType = NVTX_COLOR_ARGB;
-	eventAttrib.color = colors[color_id];
-	color_id = (color_id+1)%num_colors;
-	eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
-        eventAttrib.category = 3;
-        eventAttrib.message.ascii = "Example Range";
-//        nvtxRangeId_t rangeId = nvtxRangeStartEx(&eventAttrib);
-	if ( name != 0 )
-	{
-		eventAttrib.message.ascii = name;
-	}
-	else
-	{
-		eventAttrib.message.ascii = default_name;
-	}
-	nvtxRangePushEx(&eventAttrib);
+void rangePush(const char *const name) {
+    nvtxEventAttributes_t eventAttrib;
+    eventAttrib.version = NVTX_VERSION;
+    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+    eventAttrib.colorType = NVTX_COLOR_ARGB;
+    eventAttrib.color = colors[color_id];
+    color_id = (color_id + 1) % num_colors;
+    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
+    eventAttrib.category = 3;
+    eventAttrib.message.ascii = "Example Range";
+    //        nvtxRangeId_t rangeId = nvtxRangeStartEx(&eventAttrib);
+    if (name != 0) {
+        eventAttrib.message.ascii = name;
+    } else {
+        eventAttrib.message.ascii = default_name;
+    }
+    nvtxRangePushEx(&eventAttrib);
 }
 
-void rangePop()
-{
-	nvtxRangePop();
+void rangePop() {
+    nvtxRangePop();
 }
