f3cad5bafc49744162f0d6f8118c370317097bdf
Guilherme <g.almeida@cern.ch>
2023-04-28 11:22:03 +0200

    Rename read_spacepoints_alt to read_spacepoints

--- examples/run/cuda/seeding_example_cuda.cpp
+++ examples/run/cuda/seeding_example_cuda.cpp
@@ -13,7 +13,6 @@
 #include "traccc/efficiency/track_filter.hpp"
 #include "traccc/io/read_geometry.hpp"
 #include "traccc/io/read_spacepoints.hpp"
-#include "traccc/io/read_spacepoints_alt.hpp"
 #include "traccc/options/common_options.hpp"
 #include "traccc/options/handle_argument_errors.hpp"
 #include "traccc/options/seeding_input_options.hpp"
@@ -107,7 +106,7 @@ int seq_run(const traccc::seeding_input_config& i_cfg,
                 traccc::performance::timer t("Hit reading  (cpu)",
                                              elapsedTimes);
                 // Read the hits from the relevant event file
-                reader_output = traccc::io::read_spacepoints_alt(
+                reader_output = traccc::io::read_spacepoints(
                     event, common_opts.input_directory, surface_transforms,
                     common_opts.input_data_format, &host_mr);
             }  // stop measuring hit reading timer
--- io/src/csv/read_spacepoints.hpp
+++ io/src/csv/read_spacepoints.hpp
@@ -10,6 +10,7 @@
 // Project include(s).
 #include "traccc/edm/spacepoint.hpp"
 #include "traccc/geometry/geometry.hpp"
+#include "traccc/io/reader_edm.hpp"
 
 // VecMem include(s).
 #include <vecmem/memory/memory_resource.hpp>
@@ -23,10 +24,10 @@ namespace traccc::io::csv {
 ///
 /// @param filename The file to read the spacepoint data from
 /// @param geom The description of the detector geometry
-/// @param mr The memory resource to create the host container with
-/// @return A spacepoint (host) container
+/// @param mr The memory resource to create the host collection with
+/// @return A spacepoint (host) collection
 ///
-spacepoint_container_types::host read_spacepoints(
+spacepoint_reader_output read_spacepoints(
     std::string_view filename, const geometry& geom,
     vecmem::memory_resource* mr = nullptr);
 
