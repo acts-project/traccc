d61277b1c027889ca98d985a96cac9e23b34ea2f
beomki-yeo <beomki.yeo@gmail.com>
2023-05-12 13:36:22 +0200

    Add a plot for reduced chi2 (chi2/ndf)

--- performance/src/resolution/stat_plot_tool.cpp
+++ performance/src/resolution/stat_plot_tool.cpp
@@ -25,9 +25,12 @@ void stat_plot_tool::book(stat_plot_cache& cache) const {
 #ifdef TRACCC_HAVE_ROOT
     plot_helpers::binning b_ndf = m_cfg.var_binning.at("ndf");
     plot_helpers::binning b_chi2 = m_cfg.var_binning.at("chi2");
+    plot_helpers::binning b_reduced_chi2 = m_cfg.var_binning.at("reduced_chi2");
     plot_helpers::binning b_pval = m_cfg.var_binning.at("pval");
     cache.ndf_hist = plot_helpers::book_histo("ndf", "NDF", b_ndf);
     cache.chi2_hist = plot_helpers::book_histo("chi2", "Chi2", b_chi2);
+    cache.reduced_chi2_hist =
+        plot_helpers::book_histo("reduced_chi2", "Chi2/NDF", b_reduced_chi2);
     cache.pval_hist = plot_helpers::book_histo("pval", "p value", b_pval);
 #endif  // TRACCC_HAVE_ROOT
 }
@@ -43,6 +46,7 @@ void stat_plot_tool::fill(stat_plot_cache& cache,
     const auto& chi2 = fit_info.chi2;
     cache.ndf_hist->Fill(ndf);
     cache.chi2_hist->Fill(chi2);
+    cache.reduced_chi2_hist->Fill(chi2 / ndf);
     cache.pval_hist->Fill(ROOT::Math::chisquared_cdf_c(chi2, ndf));
 #endif  // TRACCC_HAVE_ROOT
 }
@@ -55,6 +59,7 @@ void stat_plot_tool::write(const stat_plot_cache& cache) const {
 #ifdef TRACCC_HAVE_ROOT
     cache.ndf_hist->Write();
     cache.chi2_hist->Write();
+    cache.reduced_chi2_hist->Write();
     cache.pval_hist->Write();
 #endif  // TRACCC_HAVE_ROOT
 }
