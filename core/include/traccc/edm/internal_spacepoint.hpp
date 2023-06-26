0fee6da9e6511a1a32bf8af20e01357920fd80ad
beomki-yeo <beomki.yeo@gmail.com>
2023-06-26 17:41:35 +0200

    Rollback to sqrt

--- core/include/traccc/edm/internal_spacepoint.hpp
+++ core/include/traccc/edm/internal_spacepoint.hpp
@@ -41,7 +41,7 @@ struct internal_spacepoint {
         m_x = sp.global[0] - offsetXY[0];
         m_y = sp.global[1] - offsetXY[1];
         m_z = sp.global[2];
-        m_r = std::hypot(m_x, m_y);
+        m_r = algebra::math::sqrt(m_x * m_x + m_y * m_y);
         m_phi = algebra::math::atan2(m_y, m_x);
     }
 
