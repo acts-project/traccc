e3e40d07d9380fe2c7a8adf274cf74079c9edd4b
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-03-13 17:53:52 +0100

    Starting with the re-write of the detector data handling.
    Declared an SoA container called traccc::detector_description that
    would hold all of the detector description data needed by the
    measurement and spacepoint creation algorithms. Fillable both from
    the old-style CSV files used for the TrackML detector, and from
    JSON Detray geometry configuration files.

---
+++ io/src/read_detector_description.cpp
@@ -0,0 +1,179 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Library include(s).
+#include "traccc/io/read_detector_description.hpp"
+
+#include "csv/read_surfaces.hpp"
+#include "traccc/io/read_detector.hpp"
+#include "traccc/io/read_digitization_config.hpp"
+#include "traccc/io/utils.hpp"
+
+// VecMem include(s).
+#include <vecmem/memory/host_memory_resource.hpp>
+
+// System include(s).
+#include <sstream>
+#include <stdexcept>
+
+namespace {
+
+void fill_digi_info(traccc::detector_description::host& dd,
+                    const Acts::BinUtility& data) {
+
+    // Set a hard-coded threshold for which cells should be considered for
+    // clusterization on this module / surface.
+    dd.threshold().back() = 0.f;
+
+    // Fill the new element with the digitization configuration for the
+    // surface.
+    const auto& binning_data = data.binningData();
+    dd.reference_x().back() = binning_data.at(0).min;
+    dd.reference_y().back() = binning_data.at(1).min;
+    dd.pitch_x().back() = binning_data.at(0).step;
+    dd.pitch_y().back() = binning_data.at(1).step;
+}
+
+void read_csv_dd(traccc::detector_description::host& dd,
+                 std::string_view geometry_file,
+                 const traccc::digitization_config& digi) {
+
+    // Read the geometry description as a map of surface tranformations.
+    const std::map<traccc::geometry_id, traccc::transform3> surfaces =
+        traccc::io::csv::read_surfaces(traccc::io::data_directory() +
+                                       geometry_file.data());
+
+    // Fill the detector description with information about the (sensitive)
+    // surfaces, and the digitization configurations belonging to those
+    // surfaces.
+    dd.reserve(surfaces.size());
+    for (const auto& [geom_id, transform] : surfaces) {
+
+        // Acts geometry identifier for the surface.
+        const Acts::GeometryIdentifier acts_geom_id{geom_id};
+
+        // Skip non-sensitive surfaces. They are not needed in the
+        // "detector description".
+        if (acts_geom_id.sensitive() == 0) {
+            continue;
+        }
+
+        // Add a new element to the detector description.
+        dd.resize(dd.size() + 1);
+
+        // Fill the new element with the geometry ID and the transformation of
+        // the surface in question.
+        dd.surface_link().back() = detray::geometry::barcode{geom_id};
+        dd.placement().back() = transform;
+
+        // Find the module's digitization configuration.
+        const traccc::digitization_config::Iterator digi_it =
+            digi.find(acts_geom_id);
+        if (digi_it == digi.end()) {
+            std::ostringstream msg;
+            msg << "Could not find digitization config for geometry ID: "
+                << acts_geom_id;
+            throw std::runtime_error(msg.str());
+        }
+
+        // Fill the new element with the digitization configuration for the
+        // surface.
+        fill_digi_info(dd, digi_it->segmentation);
+    }
+}
+
+std::map<std::uint64_t, detray::geometry::barcode> read_json_dd(
+    traccc::detector_description::host& dd, std::string_view geometry_file,
+    const traccc::digitization_config& digi) {
+
+    // Construct a (temporary) Detray detector object from the geometry
+    // configuration file.
+    vecmem::host_memory_resource mr;
+    traccc::default_detector::host detector{mr};
+    const traccc::default_detector::host::geometry_context ctx{};
+    traccc::io::read_detector(detector, mr, geometry_file);
+
+    // Construct the result map.
+    std::map<std::uint64_t, detray::geometry::barcode> barcode_map;
+
+    // Iterate over the surfaces of the detector.
+    const traccc::default_detector::host::surface_lookup_container& surfaces =
+        detector.surfaces();
+    dd.reserve(surfaces.size());
+    for (const auto& surface_desc : detector.surfaces()) {
+
+        // Acts geometry identifier for the surface.
+        const Acts::GeometryIdentifier acts_geom_id{surface_desc.source};
+
+        // Skip non-sensitive surfaces. They are not needed in the
+        // "detector description".
+        if (acts_geom_id.sensitive() == 0) {
+            continue;
+        }
+
+        // Extend the barcode map.
+        barcode_map[surface_desc.source] = surface_desc.barcode();
+
+        // Add a new element to the detector description.
+        dd.resize(dd.size() + 1);
+
+        // Construct a Detray surface object.
+        const detray::surface<traccc::default_detector::host> surface{
+            detector, surface_desc.barcode()};
+
+        // Fill the new element with the geometry ID and the transformation of
+        // the surface in question.
+        dd.surface_link().back() = surface_desc.barcode();
+        dd.placement().back() = surface.transform(ctx);
+
+        // Find the module's digitization configuration.
+        const traccc::digitization_config::Iterator digi_it =
+            digi.find(acts_geom_id);
+        if (digi_it == digi.end()) {
+            std::ostringstream msg;
+            msg << "Could not find digitization config for geometry ID: "
+                << acts_geom_id;
+            throw std::runtime_error(msg.str());
+        }
+
+        // Fill the new element with the digitization configuration for the
+        // surface.
+        fill_digi_info(dd, digi_it->segmentation);
+    }
+
+    // Return the barcode map.
+    return barcode_map;
+}
+
+}  // namespace
+
+namespace traccc::io {
+
+std::optional<std::map<std::uint64_t, detray::geometry::barcode> >
+read_detector_description(detector_description::host& dd,
+                          std::string_view geometry_file,
+                          std::string_view digitization_file,
+                          const data_format geometry_format,
+                          const data_format digitization_format) {
+
+    // Read the digitization configuration.
+    const digitization_config digi =
+        read_digitization_config(digitization_file, digitization_format);
+
+    // Fill the detector description with the correct type of geometry file.
+    switch (geometry_format) {
+        case data_format::json:
+            return ::read_json_dd(dd, geometry_file, digi);
+        case data_format::csv:
+            ::read_csv_dd(dd, geometry_file, digi);
+            return std::nullopt;
+        default:
+            throw std::invalid_argument("Unsupported geometry format.");
+    }
+}
+
+}  // namespace traccc::io
