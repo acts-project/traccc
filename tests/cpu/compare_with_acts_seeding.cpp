160d7e1215b759a05a8be679889af68593cbccf3
Stewart Martin-Haugh stewart.martin-haugh@stfc.ac.uk <smh@cern.ch>
2024-04-15 15:19:19 +0100

    Update to v27 and alter seed matching values

--- tests/cpu/compare_with_acts_seeding.cpp
+++ tests/cpu/compare_with_acts_seeding.cpp
@@ -324,8 +324,8 @@ TEST_P(CompareWithActsSeedingTests, Run) {
     // Ensure that ACTS and traccc give the same result
     // @TODO Uncomment the line below once acts-project/acts#2132 is merged
     // EXPECT_EQ(seeds.size(), seedVector.size());
-    EXPECT_NEAR(seeds.size(), seedVector.size(), seeds.size() * 0.0019);
-    EXPECT_TRUE(seed_match_ratio > 0.995);
+    EXPECT_NEAR(seeds.size(), seedVector.size(), seeds.size() * 0.0023);
+    EXPECT_GT(float(n_seed_match)/seeds.size(), 0.9977);
 }
 
 INSTANTIATE_TEST_SUITE_P(
