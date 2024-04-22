e8675e0052b472f1ff3d4d6c76b605a468a103b1
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-04-22 15:53:10 +0200

    Introduced host and CUDA measurement sorting algorithms.
    Did not add anything for SYCL yet, as oneDPL is still not
    working as we'd like. :-(
    
    The measurement sorting is necessary for the subsequent
    track finding algorithm. Since the current clusterization
    algorithm does not produce the measurements in a sorted
    state (yet).

---
+++ device/cuda/include/traccc/cuda/clusterization/measurement_sorting_algorithm.hpp
@@ -0,0 +1,60 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Local include(s).
+#include "traccc/cuda/utils/stream.hpp"
+
+// Project include(s).
+#include "traccc/edm/measurement.hpp"
+#include "traccc/utils/algorithm.hpp"
+
+// VecMem include(s).
+#include <vecmem/utils/copy.hpp>
+
+// System include(s).
+#include <functional>
+
+namespace traccc::cuda {
+
+/// Algorithm sorting the reconstructed measurements in their container
+///
+/// The track finding algorithm expects measurements belonging to a single
+/// detector module to be consecutive in memory. But
+/// @c traccc::cuda::clusterization_algorithm does not (currently) produce the
+/// measurements in such an ordered state. This is where this algorithm comes
+/// to the rescue.
+///
+class measurement_sorting_algorithm
+    : public algorithm<measurement_collection_types::view(
+          const measurement_collection_types::view&)> {
+
+    public:
+    /// Constructor for the algorithm
+    ///
+    /// @param copy The copy object to use in the algorithm
+    /// @param str The CUDA stream to schedule the measurement sorting in
+    ///
+    measurement_sorting_algorithm(vecmem::copy& copy, stream& str);
+
+    /// Callable operator performing the sorting on a container
+    ///
+    /// @param measurements The measurements to sort
+    ///
+    output_type operator()(const measurement_collection_types::view&
+                               measurements_view) const override;
+
+    private:
+    /// Copy object to use in the algorithm
+    std::reference_wrapper<vecmem::copy> m_copy;
+    /// CUDA stream used by the algorithm
+    std::reference_wrapper<stream> m_stream;
+
+};  // class measurement_sorting_algorithm
+
+}  // namespace traccc::cuda
---
+++ device/cuda/src/clusterization/measurement_sorting_algorithm.cu
@@ -0,0 +1,43 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Library include(s).
+#include "../utils/utils.hpp"
+#include "traccc/cuda/clusterization/measurement_sorting_algorithm.hpp"
+
+// Thrust include(s).
+#include <thrust/sort.h>
+
+namespace traccc::cuda {
+
+measurement_sorting_algorithm::measurement_sorting_algorithm(vecmem::copy& copy,
+                                                             stream& str)
+    : m_copy{copy}, m_stream{str} {}
+
+measurement_sorting_algorithm::output_type
+measurement_sorting_algorithm::operator()(
+    const measurement_collection_types::view& measurements_view) const {
+
+    // Get a convenience variable for the stream that we'll be using.
+    cudaStream_t stream = details::get_stream(m_stream);
+
+    // Get the number of measurements. This is necessary because the input
+    // container may not be fixed sized. And we can't give invalid pointers /
+    // iterators to Thrust.
+    const measurement_collection_types::view::size_type n_measurements =
+        m_copy.get().get_size(measurements_view);
+
+    // Sort the measurements in place
+    thrust::sort(thrust::cuda::par.on(stream), measurements_view.ptr(),
+                 measurements_view.ptr() + n_measurements,
+                 measurement_sort_comp());
+
+    // Return the view of the sorted measurements.
+    return measurements_view;
+}
+
+}  // namespace traccc::cuda
