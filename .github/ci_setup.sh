aeae7b8b998ce630493ee36bcfdf283459e30b93
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-05-30 15:52:48 +0200

    Updated the GPU CI builds.
    They now use the latest Acts Docker images, and set up
    their build environments in a slightly updated way.

--- .github/ci_setup.sh
+++ .github/ci_setup.sh
@@ -1,6 +1,6 @@
 # TRACCC library, part of the ACTS project (R&D line)
 #
-# (c) 2022-2023 CERN for the benefit of the ACTS project
+# (c) 2022-2024 CERN for the benefit of the ACTS project
 #
 # Mozilla Public License Version 2.0
 #
@@ -13,6 +13,14 @@
 # The platform name.
 PLATFORM_NAME=$1
 
-# Make sure that GNU Make and CTest would use all available cores.
-export MAKEFLAGS="-j`nproc`"
-export CTEST_PARALLEL_LEVEL=`nproc`
+# Make sure that the build and CTest would use all available cores.
+export CMAKE_BUILD_PARALLEL_LEVEL=`nproc`
+export CTEST_PARALLEL_LEVEL=${CMAKE_BUILD_PARALLEL_LEVEL}
+export MAKEFLAGS="-j${CMAKE_BUILD_PARALLEL_LEVEL}"
+
+# Set up the correct environment for the SYCL tests.
+if [ "${PLATFORM_NAME}" = "SYCL" ]; then
+   if [ -f "/opt/intel/oneapi/setvars.sh" ]; then
+      source /opt/intel/oneapi/setvars.sh --include-intel-llvm
+   fi
+fi
