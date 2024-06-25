a1a7a91d1b6728983c9a408480620c8ace800063
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-06-25 18:08:04 +0200

    Updated the cpu tests to traccc::detector_description.

--- tests/cpu/test_ckf_combinatorics_telescope.cpp
+++ tests/cpu/test_ckf_combinatorics_telescope.cpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2023 CERN for the benefit of the ACTS project
+ * (c) 2023-2024 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -145,11 +145,9 @@ TEST_P(CpuCkfCombinatoricsTelescopeTests, Run) {
         ASSERT_EQ(seeds.size(), n_truth_tracks);
 
         // Read measurements
-        traccc::io::measurement_reader_output readOut(&host_mr);
-        traccc::io::read_measurements(readOut, i_evt, path,
-                                      traccc::data_format::csv);
-        traccc::measurement_collection_types::host& measurements_per_event =
-            readOut.measurements;
+        traccc::measurement_collection_types::host measurements_per_event{
+            &host_mr};
+        traccc::io::read_measurements(measurements_per_event, i_evt, path);
 
         // Run finding
         auto track_candidates =
