5915831fab131bf18aa765fe354cba12a1dce012
Beomki Yeo <beomki.yeo@gmail.com>
2024-09-29 23:02:45 +0200

    Fix the starting positoin of telescope build in simulation

--- examples/simulation/simulate_telescope.cpp
+++ examples/simulation/simulate_telescope.cpp
@@ -60,7 +60,7 @@ int simulate(const traccc::opts::generation& generation_opts,
     std::vector<scalar> plane_positions;
 
     for (unsigned int i = 0; i < telescope_opts.n_planes; i++) {
-        plane_positions.push_back((i + 1) * telescope_opts.spacing);
+        plane_positions.push_back(i * telescope_opts.spacing);
     }
 
     // B field value and its type
