afefdf072e3345479378eaef5135d62fa3e5521f
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-10-30 17:51:14 +0100

    De-templated the SYCL version of spacepoint formation.
    In the same way as I did for traccc::host::silicon_pixel_spacepoint_formation_algorithm
    earlier.

---
+++ device/sycl/src/seeding/silicon_pixel_spacepoint_formation_algorithm.cpp
@@ -0,0 +1,19 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2023-2024 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Local include(s).
+#include "traccc/sycl/seeding/silicon_pixel_spacepoint_formation_algorithm.hpp"
+
+namespace traccc::sycl {
+
+silicon_pixel_spacepoint_formation_algorithm::
+    silicon_pixel_spacepoint_formation_algorithm(
+        const traccc::memory_resource& mr, vecmem::copy& copy,
+        queue_wrapper queue)
+    : m_mr(mr), m_copy(copy), m_queue(queue) {}
+
+}  // namespace traccc::sycl
