cf93ff21fef2190c3e9d80c6e4601c6321d01fa7
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2024-11-14 17:25:31 +0100

    Small track finding optimizations.
    Made the "track liveness" buffers use char instead of unsigned int.
    Since they store boolean information, char is enough. And it also
    makes memset(...) do a more expected thing on the buffers.
    
    Updated device::make_barcode_sequence not to narrow std::size_t
    into unsigned int. (Which oneAPI doesn't like.)

--- device/cuda/src/finding/finding_algorithm.cu
+++ device/cuda/src/finding/finding_algorithm.cu
@@ -148,8 +148,7 @@ finding_algorithm<stepper_t, navigator_t>::operator()(
     m_copy.setup(in_params_buffer)->ignore();
     m_copy(vecmem::get_data(seeds_buffer), vecmem::get_data(in_params_buffer))
         ->ignore();
-    vecmem::data::vector_buffer<unsigned int> param_liveness_buffer(n_seeds,
-                                                                    m_mr.main);
+    vecmem::data::vector_buffer<char> param_liveness_buffer(n_seeds, m_mr.main);
     m_copy.setup(param_liveness_buffer)->ignore();
     m_copy.memset(param_liveness_buffer, 1)->ignore();
 
@@ -215,9 +214,10 @@ finding_algorithm<stepper_t, navigator_t>::operator()(
                     m_mr.main);
             m_copy.setup(updated_params_buffer)->ignore();
 
-            vecmem::data::vector_buffer<unsigned int> updated_liveness_buffer(
+            vecmem::data::vector_buffer<char> updated_liveness_buffer(
                 n_in_params * m_cfg.max_num_branches_per_surface, m_mr.main);
             m_copy.setup(updated_liveness_buffer)->ignore();
+            m_copy.memset(updated_liveness_buffer, 0)->ignore();
 
             // Create the link map
             link_map[step] = {n_in_params * m_cfg.max_num_branches_per_surface,
