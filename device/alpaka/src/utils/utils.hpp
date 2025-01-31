018a4fa1ee119980e4ade07991a4939c49ebc45b
Stewart Martin-Haugh stewart.martin-haugh@stfc.ac.uk <smh@cern.ch>
2025-01-31 00:03:12 +0000

    Formatting

--- device/alpaka/src/utils/utils.hpp
+++ device/alpaka/src/utils/utils.hpp
@@ -21,13 +21,13 @@ using Acc = ::alpaka::AccGpuCudaRt<Dim, Idx>;
 #elif defined(ALPAKA_ACC_GPU_HIP_ENABLED)
 using Acc = ::alpaka::AccGpuHipRt<Dim, Idx>;
 #elif defined(ALPAKA_ACC_SYCL_ENABLED)
-#    if defined(ALPAKA_SYCL_ONEAPI_CPU)
-    using Acc = ::alpaka::AccCpuSycl<Dim, Idx>;
-#    elif defined(ALPAKA_SYCL_ONEAPI_FPGA)
-    using Acc = ::alpaka::AccFpgaSyclIntel<Dim, Idx>;
-#    elif defined(ALPAKA_SYCL_ONEAPI_GPU)
-    using Acc = ::alpaka::AccGpuSyclIntel<Dim, Idx>;
-#    endif
+#if defined(ALPAKA_SYCL_ONEAPI_CPU)
+using Acc = ::alpaka::AccCpuSycl<Dim, Idx>;
+#elif defined(ALPAKA_SYCL_ONEAPI_FPGA)
+using Acc = ::alpaka::AccFpgaSyclIntel<Dim, Idx>;
+#elif defined(ALPAKA_SYCL_ONEAPI_GPU)
+using Acc = ::alpaka::AccGpuSyclIntel<Dim, Idx>;
+#endif
 #elif defined(ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLED)
 using Acc = ::alpaka::AccCpuThreads<Dim, Idx>;
 #else
