247af4f791ec04ebb5256a790b9c98598aca8f76
Joana Niermann <joana.niermann@cern.ch>
2025-02-04 16:36:25 +0000

    Update to detray version v0.88.1

--- device/cuda/src/finding/kernels/specializations/types.hpp
+++ device/cuda/src/finding/kernels/specializations/types.hpp
@@ -8,11 +8,7 @@
 #pragma once
 
 #include "detray/detectors/bfield.hpp"
-#include "detray/propagator/actor_chain.hpp"
-#include "detray/propagator/actors/aborters.hpp"
-#include "detray/propagator/actors/parameter_resetter.hpp"
-#include "detray/propagator/actors/parameter_transporter.hpp"
-#include "detray/propagator/actors/pointwise_material_interactor.hpp"
+#include "detray/propagator/actors.hpp"
 #include "detray/propagator/propagator.hpp"
 #include "detray/propagator/rk_stepper.hpp"
 #include "traccc/cuda/finding/finding_algorithm.hpp"
