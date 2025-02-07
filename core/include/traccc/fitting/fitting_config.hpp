db55c11baa6993055321e32b2d0f9e20450e0432
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-02-07 10:17:17 +0100

    Add a missing include.

--- core/include/traccc/fitting/fitting_config.hpp
+++ core/include/traccc/fitting/fitting_config.hpp
@@ -1,11 +1,14 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2023-2024 CERN for the benefit of the ACTS project
+ * (c) 2023-2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
 
 #pragma once
+
+// Local include(s).
+#include "traccc/definitions/primitives.hpp"
 
 // detray include(s).
 #include "detray/definitions/pdg_particle.hpp"
