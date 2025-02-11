46471174f84016c5121b40d50277e055021ebb9f
Stewart Martin-Haugh <smh@cern.ch>
2025-02-11 17:00:40 +0100

    Make functions inline to prevent ODR violations

--- device/alpaka/include/traccc/alpaka/utils/vecmem_getter.hpp
+++ device/alpaka/include/traccc/alpaka/utils/vecmem_getter.hpp
@@ -20,109 +20,86 @@
 #include <vecmem/memory/sycl/shared_memory_resource.hpp>
 #include <vecmem/utils/sycl/copy.hpp>
 #include <vecmem/utils/sycl/queue_wrapper.hpp>
+#endif
 
-#else
+#include <memory>
+
 #include <vecmem/memory/memory_resource.hpp>
 #include <vecmem/utils/copy.hpp>
-#endif
 
 #include <alpaka/acc/Tag.hpp>
 
 #include "traccc/alpaka/utils/device_tag.hpp"
 
-// Forward declarations so we can compile the types below
-namespace vecmem {
-class host_memory_resource;
-class copy;
-namespace cuda {
-class host_memory_resource;
-class device_memory_resource;
-class managed_memory_resource;
-class copy;
-}  // namespace cuda
-namespace hip {
-class host_memory_resource;
-class device_memory_resource;
-class managed_memory_resource;
-class copy;
-}  // namespace hip
-namespace sycl {
-class host_memory_resource;
-class device_memory_resource;
-class shared_memory_resource;
-class copy;
-}  // namespace sycl
-}  // namespace vecmem
-
 namespace traccc::alpaka::vecmem {
 template <typename TTag>
-std::shared_ptr<::vecmem::memory_resource> get_host_memory_resource() {
+inline std::shared_ptr<::vecmem::memory_resource> get_host_memory_resource() {
     return std::make_shared<::vecmem::host_memory_resource>();
 }
 template <typename TTag>
-std::shared_ptr<::vecmem::memory_resource> get_device_memory_resource() {
-    return std::make_shared<::vecmem::host_memory_resource>();
-}
-
-template <typename TTag>
-std::shared_ptr<::vecmem::memory_resource> get_managed_memory_resource() {
+inline std::shared_ptr<::vecmem::memory_resource> get_device_memory_resource() {
     return std::make_shared<::vecmem::host_memory_resource>();
 }
 
 template <typename TTag>
-std::shared_ptr<::vecmem::copy> get_device_copy() {
+inline std::shared_ptr<::vecmem::memory_resource> get_managed_memory_resource() {
+    return std::make_shared<::vecmem::host_memory_resource>();
+}
+
+template <typename TTag>
+inline std::shared_ptr<::vecmem::copy> get_device_copy() {
     return std::make_shared<::vecmem::copy>();
 }
 
 #ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_host_memory_resource<::alpaka::TagGpuCudaRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::cuda::host_memory_resource>());
 }
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_device_memory_resource<::alpaka::TagGpuCudaRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::cuda::device_memory_resource>());
 }
 
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_managed_memory_resource<::alpaka::TagGpuCudaRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::cuda::managed_memory_resource>());
 }
 
 template <>
-std::shared_ptr<::vecmem::copy> get_device_copy<::alpaka::TagGpuCudaRt>() {
-    return static_pointer_cast<::vecmem::copy>(
+inline std::shared_ptr<::vecmem::copy> get_device_copy<::alpaka::TagGpuCudaRt>() {
+    return std::static_pointer_cast<::vecmem::copy>(
         std::make_shared<::vecmem::cuda::copy>());
 }
 #elif defined(ALPAKA_ACC_GPU_HIP_ENABLED)
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_host_memory_resource<::alpaka::TagGpuHipRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::hip::host_memory_resource>());
 }
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_device_memory_resource<::alpaka::TagGpuHipRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::hip::device_memory_resource>());
 }
 
 template <>
-std::shared_ptr<::vecmem::memory_resource>
+inline std::shared_ptr<::vecmem::memory_resource>
 get_managed_memory_resource<::alpaka::TagGpuHipRt>() {
     return std::static_pointer_cast<::vecmem::memory_resource>(
         std::make_shared<::vecmem::hip::managed_memory_resource>());
 }
 
 template <>
-std::shared_ptr<::vecmem::copy> get_device_copy<::alpaka::TagGpuHipRt>() {
+inline std::shared_ptr<::vecmem::copy> get_device_copy<::alpaka::TagGpuHipRt>() {
     return static_pointer_cast<::vecmem::copy>(
         std::make_shared<::vecmem::hip::copy>());
 }
