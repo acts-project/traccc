5f93fe3e9a88eeb52946fc6511ae901cd1079ccf
Attila Krasznahorkay <30694331+krasznaa@users.noreply.github.com>
2025-02-27 16:57:53 +0100

    Merge pull request #898 from krasznaa/LoggingTweaks-main-20250227
    Logging Tweaks, main branch (2025.02.27.)

--- core/include/traccc/utils/logging.hpp
+++ core/include/traccc/utils/logging.hpp
@@ -1,28 +1,41 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2021-2022 CERN for the benefit of the ACTS project
+ * (c) 2025 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
 
 #pragma once
 
+// Acts include(s).
 #include <Acts/Utilities/Logger.hpp>
 
 namespace traccc {
+
+/// Use the @c Acts::Logging namespace.
 namespace Logging = ::Acts::Logging;
 
+/// Use the @c Acts::Logger type.
 using Logger = ::Acts::Logger;
 
-inline std::unique_ptr<const Logger> getDefaultLogger(
-    const std::string& name, const Logging::Level& lvl,
-    std::ostream* log_stream = &std::cout) {
-    return ::Acts::getDefaultLogger(name, lvl, log_stream);
-}
+/// Construct a logger with default settings for this project
+///
+/// @param name the name of the log writer
+/// @param lvl the log level
+/// @param log_stream the stream to write the log to
+///
+/// @return a unique pointer to the logger
+///
+std::unique_ptr<const Logger> getDefaultLogger(
+    const std::string& name, const Logging::Level& lvl = Logging::INFO,
+    std::ostream* log_stream = &std::cout);
 
-inline const Logger& getDummyLogger() {
-    return ::Acts::getDummyLogger();
-}
+/// Construct a dummy logger that does nothing
+///
+/// @return a reference to the dummy logger
+///
+const Logger& getDummyLogger();
+
 }  // namespace traccc
 
 #define TRACCC_LOCAL_LOGGER(x) ACTS_LOCAL_LOGGER(x)
