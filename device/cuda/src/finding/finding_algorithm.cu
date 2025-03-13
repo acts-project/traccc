4c55d41374e76697e8f321d1bc47742d6edc2d83
Beomki Yeo <beomki.yeo@gmail.com>
2025-03-12 23:57:58 -0700

    Check if min_step_length_for_next_surface is larger than overstep tolerance

--- device/cuda/src/finding/finding_algorithm.cu
+++ device/cuda/src/finding/finding_algorithm.cu
@@ -73,6 +73,11 @@ finding_algorithm<stepper_t, navigator_t>::operator()(
     const typename measurement_collection_types::view& measurements,
     const bound_track_parameters_collection_types::buffer& seeds_buffer) const {
 
+    assert(m_cfg.min_step_length_for_next_surface >
+               math::fabs(m_cfg.propagation.navigation.overstep_tolerance) &&
+           "Min step length for the next surface should be higher than the "
+           "overstep tolerance");
+
     // Get a convenience variable for the stream that we'll be using.
     cudaStream_t stream = details::get_stream(m_stream);
 
