c654cd717fc7570a5af50a122adc915f9833d5d9
Joana Niermann <joana.niermann@cern.ch>
2025-04-06 17:13:19 +0000

    fix hole counting

--- tests/common/tests/toy_detector_fixture.hpp
+++ tests/common/tests/toy_detector_fixture.hpp
@@ -38,7 +38,7 @@ class ToyDetectorFixture : public testing::Test {
 
     /// Measurement smearing parameters
     static constexpr std::array<scalar, 2u> smearing{
-        10.f * traccc::unit<scalar>::um, 10.f * traccc::unit<scalar>::um};
+        50.f * traccc::unit<scalar>::um, 50.f * traccc::unit<scalar>::um};
 
     // Grid search window
     static const inline std::array<detray::dindex, 2> search_window{3u, 3u};
@@ -47,8 +47,8 @@ class ToyDetectorFixture : public testing::Test {
     static constexpr std::array<scalar, e_bound_size> stddevs = {
         1.f * traccc::unit<scalar>::mm,
         1.f * traccc::unit<scalar>::mm,
-        0.1f,
-        0.1f,
+        1.f,
+        1.f,
         1.f / traccc::unit<scalar>::GeV,
         0.01f * traccc::unit<scalar>::ns};
 
