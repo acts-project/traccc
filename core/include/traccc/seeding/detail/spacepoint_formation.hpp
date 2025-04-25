04cd6045e2ef085fb5e1f1e0af4f7e81b50010ab
Joana Niermann <joana.niermann@cern.ch>
2025-04-25 09:34:32 +0000

    Use track parameter vector directly in seeding

--- core/include/traccc/seeding/detail/spacepoint_formation.hpp
+++ core/include/traccc/seeding/detail/spacepoint_formation.hpp
@@ -24,11 +24,12 @@ TRACCC_HOST_DEVICE inline bool is_valid_measurement(const measurement& meas);
 /// @param[out] sp          The spacepoint to fill
 /// @param[in]  det         The tracking geometry
 /// @param[in]  measurement The measurement to create the spacepoint out of
+/// @param[in]  gctx        The current geometry context
 ///
 template <typename soa_t, typename detector_t>
-TRACCC_HOST_DEVICE inline void fill_pixel_spacepoint(edm::spacepoint<soa_t>& sp,
-                                                     const detector_t& det,
-                                                     const measurement& meas);
+TRACCC_HOST_DEVICE inline void fill_pixel_spacepoint(
+    edm::spacepoint<soa_t>& sp, const detector_t& det, const measurement& meas,
+    const typename detector_t::geometry_context gctx = {});
 
 }  // namespace traccc::details
 
