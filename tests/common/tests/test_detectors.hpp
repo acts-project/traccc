d76d0228b647fa375c520cb59940e7b6e23d9d2c
Joana Niermann <53186085+niermann999@users.noreply.github.com>
2025-05-29 11:58:51 +0000

    feat: update to detray version 0.97.0 (#948)
    * Update to current detray version
    
    * Disable assertions for wire chamber test

--- tests/common/tests/test_detectors.hpp
+++ tests/common/tests/test_detectors.hpp
@@ -8,9 +8,9 @@
 #pragma once
 
 // Detray include(s).
-#include <detray/test/utils/detectors/build_telescope_detector.hpp>
-#include <detray/test/utils/detectors/build_toy_detector.hpp>
-#include <detray/test/utils/detectors/build_wire_chamber.hpp>
+#include <detray/test/common/build_telescope_detector.hpp>
+#include <detray/test/common/build_toy_detector.hpp>
+#include <detray/test/common/build_wire_chamber.hpp>
 
 // useful for telescope detector creation
 #include <detray/geometry/mask.hpp>
