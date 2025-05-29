9549f03da8dde1ca4361b790e5cf87e409325448
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-05-29 21:52:20 +0200

    Migrated the CUDA ambiguity resolution to the SoA EDM.
    In a pretty brute-force way. Making use of some new features in VecMem,
    which are not part of the build just yet.

--- device/cuda/src/ambiguity_resolution/kernels/fill_track_candidates.cu
+++ device/cuda/src/ambiguity_resolution/kernels/fill_track_candidates.cu
@@ -17,34 +17,22 @@ namespace traccc::cuda::kernels {
 __global__ void fill_track_candidates(
     device::fill_track_candidates_payload payload) {
 
-    track_candidate_container_types::const_device track_candidates(
-        payload.track_candidates_view);
-
-    auto globalIndex = threadIdx.x + blockIdx.x * blockDim.x;
-
+    const auto globalIndex = details::global_index1();
     if (globalIndex >= payload.n_accepted) {
         return;
     }
 
+    // Set up the device objects.
     vecmem::device_vector<const unsigned int> sorted_ids(
         payload.sorted_ids_view);
-    track_candidate_container_types::device res_track_candidates(
-        payload.res_track_candidates_view);
+    edm::track_candidate_collection<default_algebra>::const_device
+        track_candidates(payload.track_candidates_view);
+    edm::track_candidate_collection<default_algebra>::device
+        res_track_candidates(payload.res_track_candidates_view);
 
-    // Set header
-    const auto tid = sorted_ids[globalIndex];
-    res_track_candidates.at(globalIndex).header =
-        track_candidates.at(tid).header;
-
-    // Set items
-    auto res_cands = res_track_candidates.at(globalIndex).items;
-    const auto& cands = track_candidates.at(tid).items;
-    const auto n_cands = cands.size();
-    res_cands.resize(n_cands);
-
-    for (unsigned int i = 0; i < n_cands; i++) {
-        res_cands.at(i) = cands.at(i);
-    }
+    // Copy the appropriate track candidate.
+    res_track_candidates.at(globalIndex) =
+        track_candidates.at(sorted_ids.at(globalIndex));
 }
 
 }  // namespace traccc::cuda::kernels
--- device/cuda/src/ambiguity_resolution/kernels/fill_vectors.cu
+++ device/cuda/src/ambiguity_resolution/kernels/fill_vectors.cu
@@ -21,14 +21,17 @@ namespace traccc::cuda::kernels {
 __global__ void fill_vectors(const ambiguity_resolution_config cfg,
                              device::fill_vectors_payload payload) {
 
-    track_candidate_container_types::const_device track_candidates(
-        payload.track_candidates_view);
+    const edm::track_candidate_collection<default_algebra>::const_device
+        track_candidates(payload.track_candidates_view);
 
-    auto globalIndex = threadIdx.x + blockIdx.x * blockDim.x;
-
+    const auto globalIndex = details::global_index1();
     if (globalIndex >= track_candidates.size()) {
         return;
     }
+
+    const measurement_collection_types::const_device measurements(
+        payload.measurements_view);
+    const auto track = track_candidates.at(globalIndex);
 
     vecmem::jagged_device_vector<std::size_t> meas_ids(payload.meas_ids_view);
     vecmem::device_vector<std::size_t> flat_meas_ids(
@@ -37,20 +40,18 @@ __global__ void fill_vectors(const ambiguity_resolution_config cfg,
     vecmem::device_vector<std::size_t> n_meas(payload.n_meas_view);
     vecmem::device_vector<int> status(payload.status_view);
 
-    pvals.at(globalIndex) =
-        track_candidates.at(globalIndex).header.trk_quality.pval;
+    pvals.at(globalIndex) = track.pval();
 
-    const auto& candidates = track_candidates.at(globalIndex).items;
-    const unsigned int n_cands = candidates.size();
-
-    if (n_cands < cfg.min_meas_per_track) {
+    if (track.measurement_indices().size() < cfg.min_meas_per_track) {
         status.at(globalIndex) = 0;
     } else {
-        for (const auto& cand : candidates) {
-            meas_ids.at(globalIndex).push_back(cand.measurement_id);
-            flat_meas_ids.push_back(cand.measurement_id);
+        for (const unsigned int meas_idx :
+             track_candidates.measurement_indices().at(globalIndex)) {
+            meas_ids.at(globalIndex)
+                .push_back(measurements.at(meas_idx).measurement_id);
+            flat_meas_ids.push_back(measurements.at(meas_idx).measurement_id);
         }
-        n_meas.at(globalIndex) = n_cands;
+        n_meas.at(globalIndex) = track.measurement_indices().size();
     }
 }
 }  // namespace traccc::cuda::kernels
