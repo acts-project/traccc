acts-project/traccc

commit 9444f505d62d1213da7b3a502da3a233d524d264

(CUDA) Templating Rewrite, main branch (2025.06.20.) (#1029)
* Re-wrote the CUDA CKF algorithm.

So that it would follow the same UI that all the other CKF algorithms
provide.

Had to modify the kernel specialization code a little, as it was itself
relying on definitions from the old algorithm class. In sort of a
circular way.

* Adapting the client code to the CUDA CKF rewrite.

* Re-wrote the CUDA KF algorithm.

So that it would follow the same UI that all the other KF algorithms
provide.

* Adapting the client code to the CUDA KF rewrite.

* Introduced a common set of typedefs for the CKF algorithms.

While also re-designing the templating in the functions that implement
the CKF algorithms for the different backends.

* Introduced a common typedef for the KF algorithms.

While also slightly re-designing the templating in the functions that
implement the KF algorithms for the different backends.

device/cuda/src/fitting/kernels/fill_sort_keys.cu
@@ -1,0 +1,43 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2022-2025 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Local include(s).
+#include "../../utils/cuda_error_handling.hpp"
+#include "../../utils/global_index.hpp"
+#include "fill_sort_keys.hpp"
+
+// Project include(s).
+#include "traccc/fitting/device/fill_sort_keys.hpp"
+
+namespace traccc::cuda {
+namespace kernels {
+
+__global__ void fill_sort_keys(
+    edm::track_candidate_collection<default_algebra>::const_view
+        track_candidates_view,
+    vecmem::data::vector_view<device::sort_key> keys_view,
+    vecmem::data::vector_view<unsigned int> ids_view) {
+
+    device::fill_sort_keys(details::global_index1(), track_candidates_view,
+                           keys_view, ids_view);
+}
+
+}  // namespace kernels
+
+void fill_sort_keys(const dim3& grid_size, const dim3& block_size,
+                    cudaStream_t stream,
+                    edm::track_candidate_collection<default_algebra>::const_view
+                        track_candidates_view,
+                    vecmem::data::vector_view<device::sort_key> keys_view,
+                    vecmem::data::vector_view<unsigned int> ids_view) {
+
+    kernels::fill_sort_keys<<<block_size, grid_size, 0, stream>>>(
+        track_candidates_view, keys_view, ids_view);
+    TRACCC_CUDA_ERROR_CHECK(cudaGetLastError());
+}
+
+}  // namespace traccc::cuda

benchmarks/cuda/toy_detector_cuda.cpp
@@ -6,8 +6,8 @@
  */
 
 // Project include(s).
-#include "traccc/cuda/finding/finding_algorithm.hpp"
-#include "traccc/cuda/fitting/fitting_algorithm.hpp"
+#include "traccc/cuda/finding/combinatorial_kalman_filter_algorithm.hpp"
+#include "traccc/cuda/fitting/kalman_fitting_algorithm.hpp"
 #include "traccc/cuda/seeding/seeding_algorithm.hpp"
 #include "traccc/cuda/seeding/track_params_estimation.hpp"
 #include "traccc/device/container_d2h_copy_alg.hpp"
@@ -37,16 +37,6 @@
 
 BENCHMARK_DEFINE_F(ToyDetectorBenchmark, CUDA)(benchmark::State& state) {
 
-    // Type declarations
-    using rk_stepper_type = detray::rk_stepper<
-        b_field_t::view_t, typename detector_type::algebra_type,
-        detray::constrained_step<typename detector_type::scalar_type>>;
-    using host_detector_type = traccc::default_detector::host;
-    using device_detector_type = traccc::default_detector::device;
-    using device_navigator_type = detray::navigator<const device_detector_type>;
-    using device_fitter_type =
-        traccc::kalman_fitter<rk_stepper_type, device_navigator_type>;
-
     // Memory resources used by the application.
     vecmem::cuda::host_memory_resource cuda_host_mr;
     vecmem::cuda::device_memory_resource device_mr;
@@ -59,24 +49,24 @@ BENCHMARK_DEFINE_F(ToyDetectorBenchmark, CUDA)(benchmark::State& state) {
     vecmem::cuda::async_copy async_copy{stream.cudaStream()};
 
     // Read back detector file
-    host_detector_type det{cuda_host_mr};
+    traccc::default_detector::host det{cuda_host_mr};
     traccc::io::read_detector(
         det, cuda_host_mr, sim_dir + "toy_detector_geometry.json",
         sim_dir + "toy_detector_homogeneous_material.json",
         sim_dir + "toy_detector_surface_grids.json");
 
     // B field
-    b_field_t field =
-        traccc::construct_const_bfield<host_detector_type::scalar_type>(B);
+    b_field_t field = traccc::construct_const_bfield<
+        traccc::default_detector::host::scalar_type>(B);
 
     // Algorithms
     traccc::cuda::seeding_algorithm sa_cuda(seeding_cfg, grid_cfg, filter_cfg,
                                             mr, async_copy, stream);
     traccc::cuda::track_params_estimation tp_cuda(mr, async_copy, stream);
-    traccc::cuda::finding_algorithm<rk_stepper_type, device_navigator_type>
-        device_finding(finding_cfg, mr, async_copy, stream);
-    traccc::cuda::fitting_algorithm<device_fitter_type> device_fitting(
-        fitting_cfg, mr, async_copy, stream);
+    traccc::cuda::combinatorial_kalman_filter_algorithm device_finding(
+        finding_cfg, mr, async_copy, stream);
+    traccc::cuda::kalman_fitting_algorithm device_fitting(fitting_cfg, mr,
+                                                          async_copy, stream);
 
     // Copy detector to device
     auto det_buffer = detray::get_buffer(det, device_mr, copy);

core/CMakeLists.txt
@@ -68,7 +68,8 @@ traccc_add_library( traccc_core core TYPE SHARED
   "include/traccc/finding/finding_config.hpp"
   "include/traccc/finding/actors/ckf_aborter.hpp"
   "include/traccc/finding/actors/interaction_register.hpp"
-  "include/traccc/finding/details/find_tracks.hpp"
+  "include/traccc/finding/details/combinatorial_kalman_filter_types.hpp"
+  "include/traccc/finding/details/combinatorial_kalman_filter.hpp"
   "include/traccc/finding/combinatorial_kalman_filter_algorithm.hpp"
   "src/finding/combinatorial_kalman_filter_algorithm.cpp"
   "src/finding/combinatorial_kalman_filter_algorithm_constant_field_default_detector.cpp"
@@ -80,7 +81,8 @@ traccc_add_library( traccc_core core TYPE SHARED
   "include/traccc/fitting/kalman_filter/kalman_step_aborter.hpp"
   "include/traccc/fitting/kalman_filter/statistics_updater.hpp"
   "include/traccc/fitting/kalman_filter/two_filters_smoother.hpp"
-  "include/traccc/fitting/details/fit_tracks.hpp"
+  "include/traccc/fitting/details/kalman_fitting_types.hpp"
+  "include/traccc/fitting/details/kalman_fitting.hpp"
   "include/traccc/fitting/kalman_fitting_algorithm.hpp"
   "src/fitting/kalman_fitting_algorithm.cpp"
   "src/fitting/kalman_fitting_algorithm_constant_field_default_detector.cpp"

core/include/traccc/finding/details/combinatorial_kalman_filter.hpp
@@ -13,6 +13,7 @@
 #include "traccc/finding/actors/ckf_aborter.hpp"
 #include "traccc/finding/actors/interaction_register.hpp"
 #include "traccc/finding/candidate_link.hpp"
+#include "traccc/finding/details/combinatorial_kalman_filter_types.hpp"
 #include "traccc/finding/finding_config.hpp"
 #include "traccc/fitting/kalman_filter/gain_matrix_updater.hpp"
 #include "traccc/fitting/kalman_filter/is_line_visitor.hpp"
@@ -41,8 +42,8 @@ namespace traccc::host::details {
 /// specializations, to find tracks on top of a specific detector type, magnetic
 /// field type, and track finding configuration.
 ///
-/// @tparam stepper_t The stepper type used for the track propagation
-/// @tparam navigator_t The navigator type used for the track navigation
+/// @tparam detector_t The (host) detector type to use
+/// @tparam bfield_t   The magnetic field type to use
 ///
 /// @param det               The detector object
 /// @param field             The magnetic field object
@@ -55,12 +56,10 @@ namespace traccc::host::details {
 ///
 /// @return A container of the found track candidates
 ///
-template <typename stepper_t, typename navigator_t>
-edm::track_candidate_collection<
-    typename navigator_t::detector_type::algebra_type>::host
-find_tracks(
-    const typename navigator_t::detector_type& det,
-    const typename stepper_t::magnetic_field_type& field,
+template <typename detector_t, typename bfield_t>
+edm::track_candidate_collection<default_algebra>::host
+combinatorial_kalman_filter(
+    const detector_t& det, const bfield_t& field,
     const measurement_collection_types::const_view& measurements_view,
     const bound_track_parameters_collection_types::const_view& seeds_view,
     const finding_config& config, vecmem::memory_resource& mr,
@@ -70,29 +69,15 @@ find_tracks(
                math::fabs(config.propagation.navigation.overstep_tolerance) &&
            "Min step length for the next surface should be higher than the "
            "overstep tolerance");
+    assert(config.min_track_candidates_per_track >= 1);
+
+    /// The algebra type
+    using algebra_type = typename detector_t::algebra_type;
+    /// The scalar type
+    using scalar_type = detray::dscalar<algebra_type>;
 
     // Create a logger.
     auto logger = [&log]() -> const Logger& { return log; };
-
-    /*****************************************************************
-     * Types used by the track finding
-     *****************************************************************/
-
-    using algebra_type = typename navigator_t::detector_type::algebra_type;
-    using scalar_type = detray::dscalar<algebra_type>;
-
-    using transporter_type = detray::parameter_transporter<algebra_type>;
-    using interactor_type = detray::pointwise_material_interactor<algebra_type>;
-
-    using actor_type = detray::actor_chain<
-        detray::pathlimit_aborter<scalar_type>, transporter_type,
-        interaction_register<interactor_type>, interactor_type,
-        detray::momentum_aborter<scalar_type>, ckf_aborter>;
-
-    using propagator_type =
-        detray::propagator<stepper_t, navigator_t, actor_type>;
-
-    assert(config.min_track_candidates_per_track >= 1);
 
     /*****************************************************************
      * Measurement Operations
@@ -146,7 +131,8 @@ find_tracks(
     std::vector<std::pair<unsigned int, unsigned int>> tips;
 
     // Create propagator
-    propagator_type propagator(config.propagation);
+    traccc::details::ckf_propagator_t<detector_t, bfield_t> propagator(
+        config.propagation);
 
     // Create the input seeds container.
     bound_track_parameters_collection_types::const_device seeds{seeds_view};
@@ -218,10 +204,9 @@ find_tracks(
 
             // Apply interactor
             if (sf.has_material()) {
-                const typename navigator_t::detector_type::geometry_context
-                    ctx{};
-                typename interactor_type::state interactor_state;
-                interactor_type{}.update(
+                const typename detector_t::geometry_context ctx{};
+                traccc::details::ckf_interactor_t::state interactor_state;
+                traccc::details::ckf_interactor_t{}.update(
                     ctx,
                     detail::correct_particle_hypothesis(config.ptc_hypothesis,
                                                         in_param),
@@ -364,7 +349,8 @@ find_tracks(
 
             const auto& param = updated_params[link_id];
             // Create propagator state
-            typename propagator_type::state propagation(param, field, det);
+            typename traccc::details::ckf_propagator_t<
+                detector_t, bfield_t>::state propagation(param, field, det);
             propagation.set_particle(detail::correct_particle_hypothesis(
                 config.ptc_hypothesis, param));
 
@@ -373,8 +359,9 @@ find_tracks(
                     config.propagation.stepping.step_constraint);
 
             typename detray::pathlimit_aborter<scalar_type>::state s0;
-            typename interactor_type::state s2;
-            typename interaction_register<interactor_type>::state s1{s2};
+            traccc::details::ckf_interactor_t::state s2;
+            typename interaction_register<
+                traccc::details::ckf_interactor_t>::state s1{s2};
             typename detray::momentum_aborter<scalar_type>::state s3{};
             typename ckf_aborter::state s4;
             // Update the actor config

core/include/traccc/finding/details/combinatorial_kalman_filter_types.hpp
@@ -1,0 +1,62 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2025 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Local include(s).
+#include "traccc/definitions/common.hpp"
+#include "traccc/definitions/primitives.hpp"
+#include "traccc/finding/actors/ckf_aborter.hpp"
+#include "traccc/finding/actors/interaction_register.hpp"
+
+// Detray include(s).
+#include <detray/navigation/navigator.hpp>
+#include <detray/propagator/actor_chain.hpp>
+#include <detray/propagator/actors/aborters.hpp>
+#include <detray/propagator/actors/parameter_transporter.hpp>
+#include <detray/propagator/actors/pointwise_material_interactor.hpp>
+#include <detray/propagator/constrained_step.hpp>
+#include <detray/propagator/propagator.hpp>
+#include <detray/propagator/rk_stepper.hpp>
+
+// System include(s).
+#include <type_traits>
+
+namespace traccc::details {
+
+/// Stepper used in the Combinatorial Kalman Filter (CKF)
+///
+/// @tparam bfield_t The type of magnetic field to use
+///
+template <typename bfield_t>
+using ckf_stepper_t =
+    detray::rk_stepper<bfield_t, traccc::default_algebra,
+                       detray::constrained_step<traccc::scalar>>;
+
+/// Interactor used in the Combinatorial Kalman Filter (CKF)
+using ckf_interactor_t =
+    detray::pointwise_material_interactor<traccc::default_algebra>;
+
+/// Actor chain used in the Combinatorial Kalman Filter (CKF)
+using ckf_actor_chain_t =
+    detray::actor_chain<detray::pathlimit_aborter<traccc::scalar>,
+                        detray::parameter_transporter<traccc::default_algebra>,
+                        interaction_register<ckf_interactor_t>,
+                        ckf_interactor_t,
+                        detray::momentum_aborter<traccc::scalar>, ckf_aborter>;
+
+/// Propagator type used in the Combinatorial Kalman Filter (CKF)
+///
+/// @tparam detector_t The detector type to use
+///
+template <typename detector_t, typename bfield_t>
+using ckf_propagator_t =
+    detray::propagator<ckf_stepper_t<bfield_t>,
+                       detray::navigator<std::add_const_t<detector_t>>,
+                       ckf_actor_chain_t>;
+
+}  // namespace traccc::details

core/include/traccc/fitting/details/kalman_fitting.hpp
@@ -29,7 +29,6 @@ namespace traccc::host::details {
 ///       jagged vector are created using the default memory resource.
 ///
 /// @tparam fitter_t The fitter type used for the track fitting
-/// @tparam algebra_t The algebra type used for the track fitting
 ///
 /// @param[in] fitter           The fitter object to use on the track candidates
 /// @param[in] track_container  All track candidates to fit
@@ -37,25 +36,26 @@ namespace traccc::host::details {
 ///
 /// @return A container of the fitted track states
 ///
-template <typename algebra_t, typename fitter_t>
-track_state_container_types::host fit_tracks(
+template <typename fitter_t>
+track_state_container_types::host kalman_fitting(
     fitter_t& fitter,
-    const typename edm::track_candidate_container<algebra_t>::const_view&
-        track_container,
+    const typename edm::track_candidate_container<
+        typename fitter_t::algebra_type>::const_view& track_container,
     vecmem::memory_resource& mr, vecmem::copy& copy) {
 
     // Create the input container(s).
     const measurement_collection_types::const_device measurements{
         track_container.measurements};
-    const typename edm::track_candidate_collection<algebra_t>::const_device
-        track_candidates{track_container.tracks};
+    const typename edm::track_candidate_collection<
+        typename fitter_t::algebra_type>::const_device track_candidates{
+        track_container.tracks};
 
     // Create the output container.
     track_state_container_types::host result{&mr};
 
     // Iterate over the tracks,
     for (typename edm::track_candidate_collection<
-             algebra_t>::const_device::size_type i = 0;
+             typename fitter_t::algebra_type>::const_device::size_type i = 0;
          i < track_candidates.size(); ++i) {
 
         // Make a vector of track states for this track.

core/include/traccc/fitting/details/kalman_fitting_types.hpp
@@ -1,0 +1,34 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2025 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Local include(s).
+#include "traccc/fitting/kalman_filter/kalman_fitter.hpp"
+
+// Detray include(s).
+#include <detray/navigation/navigator.hpp>
+#include <detray/propagator/constrained_step.hpp>
+#include <detray/propagator/rk_stepper.hpp>
+
+// System include(s).
+#include <type_traits>
+
+namespace traccc::details {
+
+/// Kalman fitter type for a specific detector and magnetic field type
+///
+/// @tparam detector_t The detector type to use
+/// @tparam bfield_t   The magnetic field type to use
+///
+template <typename detector_t, typename bfield_t>
+using kalman_fitter_t = kalman_fitter<
+    detray::rk_stepper<bfield_t, typename detector_t::algebra_type,
+                       detray::constrained_step<traccc::scalar>>,
+    detray::navigator<std::add_const_t<detector_t>>>;
+
+}  // namespace traccc::details

core/include/traccc/utils/detector_type_utils.hpp
@@ -1,32 +1,0 @@
-/** TRACCC library, part of the ACTS project (R&D line)
- *
- * (c) 2025 CERN for the benefit of the ACTS project
- *
- * Mozilla Public License Version 2.0
- */
-
-#pragma once
-
-#include <covfie/core/field.hpp>
-#include <traccc/fitting/kalman_filter/kalman_fitter.hpp>
-#include <traccc/utils/bfield.hpp>
-
-#include "traccc/utils/propagation.hpp"
-
-namespace traccc {
-/// Helper type constructors to facilitate the consistent instantiation of
-/// tools such as steppers and fitters for specific detectors.
-template <typename detector_t>
-using navigator_for_t = detray::navigator<const detector_t>;
-
-template <typename detector_t>
-using stepper_for_t = ::detray::rk_stepper<
-    typename ::covfie::field<
-        const_bfield_backend_t<typename detector_t::scalar_type>>::view_t,
-    typename detector_t::algebra_type,
-    ::detray::constrained_step<typename detector_t::scalar_type>>;
-
-template <typename detector_t>
-using fitter_for_t =
-    kalman_fitter<stepper_for_t<detector_t>, navigator_for_t<detector_t>>;
-}  // namespace traccc

commit 642292ebde09befbac0208d3033b5461a1aef438

Fix CUDA kernel launch parameter bug in fitter
This commit fixes a bug introduced in #1029 where the block size and
grid size for a kernel were incorrectly swapped around, leading to an
invalid launch configuration error.

device/cuda/src/fitting/kernels/fill_sort_keys.cu
@@ -35,7 +35,7 @@ void fill_sort_keys(const dim3& grid_size, const dim3& block_size,
                     vecmem::data::vector_view<device::sort_key> keys_view,
                     vecmem::data::vector_view<unsigned int> ids_view) {
 
-    kernels::fill_sort_keys<<<block_size, grid_size, 0, stream>>>(
+    kernels::fill_sort_keys<<<grid_size, block_size, 0, stream>>>(
         track_candidates_view, keys_view, ids_view);
     TRACCC_CUDA_ERROR_CHECK(cudaGetLastError());
 }