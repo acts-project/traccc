75db0d4786f5f43a39a41e058701f35e2efc5182
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-06-23 19:07:06 +0200

    Added inhomogeneous field CUDA algorithms.

---
+++ device/cuda/src/finding/combinatorial_kalman_filter_algorithm_telescope_detector.cu
@@ -0,0 +1,40 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2025 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Local include(s).
+#include "../utils/bfield.cuh"
+#include "combinatorial_kalman_filter.cuh"
+#include "traccc/cuda/finding/combinatorial_kalman_filter_algorithm.hpp"
+
+// System include(s).
+#include <stdexcept>
+
+namespace traccc::cuda {
+
+combinatorial_kalman_filter_algorithm::output_type
+combinatorial_kalman_filter_algorithm::operator()(
+    const telescope_detector::view& det, const bfield& field,
+    const measurement_collection_types::const_view& measurements,
+    const bound_track_parameters_collection_types::const_view& seeds) const {
+
+    // Perform the track finding using the templated implementation.
+    if (field.is<const_bfield_backend_t<scalar>>()) {
+        return details::combinatorial_kalman_filter<telescope_detector::device>(
+            det, field.as<const_bfield_backend_t<scalar>>(), measurements,
+            seeds, m_config, m_mr, m_copy, logger(), m_stream, m_warp_size);
+    } else if (field.is<cuda::inhom_bfield_backend_t<scalar>>()) {
+        return details::combinatorial_kalman_filter<telescope_detector::device>(
+            det, field.as<cuda::inhom_bfield_backend_t<scalar>>(), measurements,
+            seeds, m_config, m_mr, m_copy, logger(), m_stream, m_warp_size);
+    } else {
+        throw std::invalid_argument(
+            "Unsupported b-field type received in "
+            "traccc::cuda::combinatorial_kalman_filter_algorithm");
+    }
+}
+
+}  // namespace traccc::cuda
