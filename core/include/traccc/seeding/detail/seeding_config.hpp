e12540213feb59b529ae2755d20c80569f115b60
Stephen Nicholas Swatman <stephen.nicholas.swatman@cern.ch>
2025-07-24 17:05:35 +0200

    Add maximum Z distance cut in seeding
    This commit adds a new cut based on the distance in Z between doublets.

--- core/include/traccc/seeding/detail/seeding_config.hpp
+++ core/include/traccc/seeding/detail/seeding_config.hpp
@@ -45,6 +45,9 @@ struct seedfinder_config {
     // maximum distance in mm in r between two measurements within one seed
     float deltaRMax = 80 * unit<float>::mm;
 
+    // maximum distance in mm in z between measurements in one seed
+    float deltaZMax = 450 * unit<float>::mm;
+
     // FIXME: this is not used yet
     //        float upperPtResolutionPerSeed = 20* Acts::GeV;
 
