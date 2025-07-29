77a7b37938d447793a025dc0c18222aa807320a1
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2025-07-29 09:29:05 +0200

    Taught the script about the G100/G200 pipelines.

--- extras/traccc_itk_throughput_mt_profiler.sh
+++ extras/traccc_itk_throughput_mt_profiler.sh
@@ -29,6 +29,7 @@ usage() {
    echo "  -r <repetitions>     The number of repetitions in the test"
    echo "  -e <eventMultiplier> Multiplier for the number of events per thread"
    echo "  -c <csvFile>         Name of the output CSV file"
+   echo "  -y <throughputType>  Type of throughput test to run (traccc/g200/g100)"
    echo "  -h                   Print this help"
    echo ""
 }
@@ -41,7 +42,8 @@ TRACCC_MAX_THREADS=${TRACCC_MAX_THREADS:-$(nproc)}
 TRACCC_THREAD_STEP=${TRACCC_THREAD_STEP:-1}
 TRACCC_REPETITIONS=${TRACCC_REPETITIONS:-5}
 TRACCC_CSV_FILE=${TRACCC_CSV_FILE:-"output.csv"}
-while getopts ":x:i:m:t:r:c:h" opt; do
+TRACCC_THROUGPUT_TYPE=${TRACCC_THROUGPUT_TYPE:-"traccc"}
+while getopts ":x:i:m:t:r:c:y:h" opt; do
    case $opt in
       x)
          TRACCC_EXECUTABLE=$OPTARG
@@ -64,6 +66,9 @@ while getopts ":x:i:m:t:r:c:h" opt; do
       c)
          TRACCC_CSV_FILE=$OPTARG
          ;;
+      y)
+         TRACCC_THROUGPUT_TYPE=$OPTARG
+         ;;
       h)
          usage
          exit 0
@@ -83,19 +88,67 @@ done
 
 # Print the configuration received.
 echo "Using configuration:"
-echo "   EXECUTABLE  : ${TRACCC_EXECUTABLE}"
-echo "   INPUT_DIR   : ${TRACCC_INPUT_DIR}"
-echo "   MIN_THREADS : ${TRACCC_MIN_THREADS}"
-echo "   MAX_THREADS : ${TRACCC_MAX_THREADS}"
-echo "   THREAD_STEP : ${TRACCC_THREAD_STEP}"
-echo "   REPETITIONS : ${TRACCC_REPETITIONS}"
-echo "   CSV_FILE    : ${TRACCC_CSV_FILE}"
+echo "   EXECUTABLE      : ${TRACCC_EXECUTABLE}"
+echo "   INPUT_DIR       : ${TRACCC_INPUT_DIR}"
+echo "   MIN_THREADS     : ${TRACCC_MIN_THREADS}"
+echo "   MAX_THREADS     : ${TRACCC_MAX_THREADS}"
+echo "   THREAD_STEP     : ${TRACCC_THREAD_STEP}"
+echo "   REPETITIONS     : ${TRACCC_REPETITIONS}"
+echo "   CSV_FILE        : ${TRACCC_CSV_FILE}"
+echo "   THROUGHPUT_TYPE : ${TRACCC_THROUGPUT_TYPE}"
 
 # Check whether the output file already exists. Refuse to overwrite existing
 # files.
 if [[ -f "${TRACCC_CSV_FILE}" ]]; then
    echo "***"
    echo "*** Will not overwrite ${TRACCC_CSV_FILE}!"
+   echo "***"
+   exit 1
+fi
+
+# Additional flags tuning the cuts for the G100/G200 pipelines.
+G200_CUTS=(--seedfinder-z-range=-3000.:3000.
+           --seedfinder-r-range=33.:320.
+           --seedfinder-vertex-range=-200.:200.
+           --seedfinder-minPt=0.9
+           --seedfinder-cotThetaMax=27.2899
+           --seedfinder-deltaR-range=20.:200.
+           --seedfinder-impactMax=10.
+           --seedfinder-sigmaScattering=3.
+           --seedfinder-maxPtScattering=10.
+           --seedfinder-maxSeedsPerSpM=1
+           --max-num-branches-per-seed=3
+           --max-num-branches-per-surface=5
+           --track-candidates-range=7:20
+           --min-step-length-for-next-surface=0.5
+           --max-step-counts-for-next-surface=100
+           --chi2-max=10.
+           --max-num-skipping-per-cand=2
+           --stepping-min-stepsize=0.0001
+           --rk-tolerance-mm=0.0001
+           --stepping-path-limit=5.
+           --stepping-max-rk-updates=10000
+           --stepping-use-mean-loss=1
+           --stepping-use-eloss-gradient=0
+           --stepping-use-field-gradient=0
+           --stepping-do-covariance-transport=1
+           --overstep-tolerance-um=-300.
+           --min-mask-tolerance-mm=0.00001
+           --max-mask-tolerance-mm=3.
+           --search-window=0:0)
+
+G100_CUTS=${G200_CUTS[@]}
+G100_CUTS+=(--reco-stage=seeding)
+
+# Select which of these flags to use.
+TRACCC_CUTS=()
+if [[ "${TRACCC_THROUGPUT_TYPE}" == "g200" ]]; then
+   TRACCC_CUTS=${G200_CUTS[@]}
+elif [[ "${TRACCC_THROUGPUT_TYPE}" == "g100" ]]; then
+   TRACCC_CUTS=${G100_CUTS[@]}
+elif [[ "${TRACCC_THROUGPUT_TYPE}" != "traccc" ]]; then
+   echo "***"
+   echo "*** Unknown throughput type: '${TRACCC_THROUGPUT_TYPE}'"
    echo "***"
    exit 1
 fi
@@ -138,7 +191,8 @@ for NTHREAD in $(seq ${TRACCC_MIN_THREADS} ${TRACCC_THREAD_STEP} ${TRACCC_MAX_TH
             --cpu-threads=${NTHREAD}                                           \
             --cold-run-events=$((5*${NTHREAD}))                                \
             --processed-events=$((100*${NTHREAD}))                             \
-            --log-file="${TRACCC_CSV_FILE}"
+            --log-file="${TRACCC_CSV_FILE}"                                    \
+            ${TRACCC_CUTS[@]}
       done
    done
 done
