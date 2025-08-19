7c41bba75b9ff25f96b6b1bd00f1c4984ad0c844
Mark <markbrettel@cern.ch>
2025-08-19 20:43:26 +0200

    GBTS config improvments

--- core/include/traccc/gbts_seeding/gbts_seeding_config.hpp
+++ core/include/traccc/gbts_seeding/gbts_seeding_config.hpp
@@ -22,25 +22,21 @@ namespace traccc::device {
 //where to put these definitions?
 struct gbts_layerInfo {
 	std::vector<bool> isEndcap;
-	std::vector<int> etaBin0;
-	std::vector<int> numBins;
-	std::vector<float> minEta;
-	std::vector<float> delatEta;
+	//etaBin0 and numBins
+	std::vector<std::pair<int, int>> info;
+	//minEta and deltaEta
+	std::vector<std::pair<float, float>> geo;
 
 	void reserve(int n) {
 		isEndCap.reserve(n);
-		etaBin0.reserve(n);
-		numBins.reserve(n);
-		minEta.reserve(n);
-		deltaEta.reserve(n);
+		info.reserve(n);
+		geo.reserve(n);
 	}
 
-	void addLayer(bool isEnd, int firstBin, int nBins, float mEta, float dEta) {
+	void addLayer(bool isEnd, int firstBin, int nBins, float minEta, float etaBinWidth) {
 		isEndCap.push_back(isEnd);
-		etaBin0.push_back(firstBin);
-		numBins.push_back(nBins);
-		minEta.push_back(mEta);
-		deltaEta.push_back(dEta);
+		info.push_back(std::make_pair(nBins, firstBin));
+		geo.push_back(std::make_pair(minEta, etaBinWidth));
 	}
 };
 
@@ -65,19 +61,18 @@ enum class gbts_consts : unsigned short {
 namespace traccc {
 
 struct gbts_seedfinder_config {
-	gbts_seedfinder_config() = delete;
-	gbts_seedfinder_config(const std::vector<std::pair<int, int>>& binTables, const std::vector<device::gbts_layerInfo>& layerInfo,
+	setLinkingScheme(const std::vector<std::pair<int, int>>& binTables, const std::vector<device::gbts_layerInfo>& layerInfo,
               const std::vector<std::pair<uint64_t, int>>& detrayBarcodeBinning, float minPt);
 
 	//layer linking and geometry	
 	std::vector<std::pair<int, int>> binTables{};
-	std::vector<device::gbts_layerInfo> layerInfo{};
+	traccc::device::gbts_layerInfo layerInfo{};
 	unsigned int nLayers = 0;	
 
 	std::shared_ptr<int[]> volumeToLayerMap{};
 	unsigned int maxVolIndex = 0;	
 
-	std::vector<std::array<unsigned int, 2>> surfaceToLayerMap;
+	std::vector<std::array<unsigned int, 2>> surfaceToLayerMap{};
 	unsigned int surfaceMapSize = 0;	
 
 	//tuned for 900 MeV pT cut and scaled by input minPt	
