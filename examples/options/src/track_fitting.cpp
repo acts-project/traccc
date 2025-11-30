bdcfc7e9ff558c95a182ece06e20d2df26613c20
Joana Niermann <joana.niermann@cern.ch>
2025-11-30 18:32:40 +0100

    Enable the momentum aborter in the Kalman Fitter

--- examples/options/src/track_fitting.cpp
+++ examples/options/src/track_fitting.cpp
@@ -49,6 +49,19 @@ track_fitting::track_fitting() : interface("Track Fitting Options") {
         po::value(&m_config.backward_filter_mask_tolerance)
             ->default_value(m_config.backward_filter_mask_tolerance),
         "Mask tolerance for the backward filter");
+    m_desc.add_options()(
+        "min-total-momentum",
+        po::value(&m_config.min_p)->default_value(m_config.min_p),
+        "Minimum total track momentum [GeV]");
+    m_desc.add_options()(
+        "min-transverse-momentum",
+        po::value(&m_config.min_pT)->default_value(m_config.min_pT),
+        "Minimum transverse track momentum [GeV]");
+}
+
+void track_fitting::read(const po::variables_map &) {
+    m_config.min_p *= traccc::unit<float>::GeV;
+    m_config.min_pT *= traccc::unit<float>::GeV;
 }
 
 track_fitting::operator fitting_config() const {
@@ -80,6 +93,12 @@ std::unique_ptr<configuration_printable> track_fitting::as_printable() const {
     cat->add_child(std::make_unique<configuration_kv_pair>(
         "Mask tolerance for the backward filter",
         std::to_string(m_config.backward_filter_mask_tolerance)));
+    cat->add_child(std::make_unique<configuration_kv_pair>(
+        "Minimum pT",
+        std::format("{} GeV", m_config.min_pT / traccc::unit<float>::GeV)));
+    cat->add_child(std::make_unique<configuration_kv_pair>(
+        "Minimum p",
+        std::format("{} GeV", m_config.min_p / traccc::unit<float>::GeV)));
 
     return cat;
 }
