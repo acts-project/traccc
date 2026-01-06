35c5d050c99cbe6d42408039fc06f7478ff0ec2d
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-01-06 11:08:58 +0100

    Added some missing include statements.

--- device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
+++ device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2022-2025 CERN for the benefit of the ACTS project
+ * (c) 2022-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -16,6 +16,9 @@
 
 // VecMem include(s).
 #include <vecmem/containers/data/vector_view.hpp>
+
+// System include(s).
+#include <optional>
 
 namespace traccc::device {
 
