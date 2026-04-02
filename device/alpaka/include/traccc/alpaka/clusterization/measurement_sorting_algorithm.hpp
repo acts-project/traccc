dae6b1e0b1c076e2b4b090f54ca5a78a503e7efc
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-04-02 14:48:40 +0200

    Adapted traccc::alpaka to the cell+measurement concretization.

--- device/alpaka/include/traccc/alpaka/clusterization/measurement_sorting_algorithm.hpp
+++ device/alpaka/include/traccc/alpaka/clusterization/measurement_sorting_algorithm.hpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2024 CERN for the benefit of the ACTS project
+ * (c) 2024-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -35,8 +35,8 @@ namespace traccc::alpaka {
 /// to the rescue.
 ///
 class measurement_sorting_algorithm
-    : public algorithm<edm::measurement_collection<default_algebra>::buffer(
-          const edm::measurement_collection<default_algebra>::const_view&)>,
+    : public algorithm<edm::measurement_collection::buffer(
+          const edm::measurement_collection::const_view&)>,
       public messaging {
 
     public:
@@ -54,8 +54,8 @@ class measurement_sorting_algorithm
     /// @param measurements The measurements to sort
     ///
     [[nodiscard]] output_type operator()(
-        const edm::measurement_collection<default_algebra>::const_view&
-            measurements) const override;
+        const edm::measurement_collection::const_view& measurements)
+        const override;
 
     private:
     // The memory resource(s) to use
