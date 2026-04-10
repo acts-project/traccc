d4921b7371e18a533fb9eafc757f771726b88823
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-04-10 10:42:48 +0200

    Concretized edm::spacepoint.
    So that its memory layout would not depend on which algebra the code
    is built with.
    
    Adapted all client code to this change.

--- core/include/traccc/utils/detray_conversion.hpp
+++ core/include/traccc/utils/detray_conversion.hpp
@@ -28,6 +28,16 @@ template <detray::concepts::algebra ALGEBRA_TYPE>
 TRACCC_HOST_DEVICE std::array<float, 2u> to_float_array(
     const detray::dpoint2D<ALGEBRA_TYPE>& point);
 
+/// Convert an algebra specific 3D point to a float array of size 3
+///
+/// @tparam ALGEBRA_TYPE The algebra type of the input point
+/// @param point The input 3D point to be converted
+/// @return An @c std::array of size 3 containing the converted float values
+///
+template <detray::concepts::algebra ALGEBRA_TYPE>
+TRACCC_HOST_DEVICE std::array<float, 3u> to_float_array(
+    const detray::dpoint3D<ALGEBRA_TYPE>& point);
+
 /// Convert a float array of size 2 to an algebra specific 2D point
 ///
 /// @tparam ALGEBRA_TYPE The algebra type of the output point
@@ -37,6 +47,16 @@ TRACCC_HOST_DEVICE std::array<float, 2u> to_float_array(
 template <detray::concepts::algebra ALGEBRA_TYPE>
 TRACCC_HOST_DEVICE detray::dpoint2D<ALGEBRA_TYPE> to_dpoint2D(
     const std::array<float, 2u>& arr);
+
+/// Convert a float array of size 3 to an algebra specific 3D point
+///
+/// @tparam ALGEBRA_TYPE The algebra type of the output point
+/// @param arr The input @c std::array of size 3 containing the float values
+/// @return A 3D point of type @c detray::dpoint3D with the converted values
+///
+template <detray::concepts::algebra ALGEBRA_TYPE>
+TRACCC_HOST_DEVICE detray::dpoint3D<ALGEBRA_TYPE> to_dpoint3D(
+    const std::array<float, 3u>& arr);
 
 }  // namespace traccc::utils
 
