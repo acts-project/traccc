86ef24a3987b16ee5f14bfba8abce48101fef1aa
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-06-01 17:17:09 +0200

    Updated to the latest clusterization code organization.

--- device/hip/src/clusterization/kernels/ccl_kernel.hpp
+++ device/hip/src/clusterization/kernels/ccl_kernel.hpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2022-2025 CERN for the benefit of the ACTS project
+ * (c) 2022-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -10,10 +10,10 @@
 // Project include(s).
 #include "traccc/clusterization/clustering_config.hpp"
 #include "traccc/clusterization/device/ccl_kernel_definitions.hpp"
-#include "traccc/definitions/primitives.hpp"
 #include "traccc/edm/measurement_collection.hpp"
 #include "traccc/edm/silicon_cell_collection.hpp"
-#include "traccc/geometry/silicon_detector_description.hpp"
+#include "traccc/geometry/detector_conditions_description.hpp"
+#include "traccc/geometry/detector_design_description.hpp"
 
 // VecMem include(s).
 #include <vecmem/containers/data/vector_view.hpp>
@@ -24,13 +24,14 @@ namespace traccc::hip::kernels {
 __global__ void ccl_kernel(
     const clustering_config cfg,
     const edm::silicon_cell_collection::const_view cells_view,
-    const silicon_detector_description::const_view det_descr_view,
-    edm::measurement_collection<default_algebra>::view measurements_view,
-    vecmem::data::vector_view<unsigned int> cell_links,
-    vecmem::data::vector_view<device::details::index_t> f_backup_view,
-    vecmem::data::vector_view<device::details::index_t> gf_backup_view,
+    const detector_design_description::const_view det_descr_view,
+    const detector_conditions_description::const_view det_cond_view,
+    edm::measurement_collection::view measurements_view,
+    vecmem::data::vector_view<device::details::fallback_index_t> f_backup_view,
+    vecmem::data::vector_view<device::details::fallback_index_t> gf_backup_view,
     vecmem::data::vector_view<unsigned char> adjc_backup_view,
-    vecmem::data::vector_view<device::details::index_t> adjv_backup_view,
+    vecmem::data::vector_view<device::details::fallback_index_t>
+        adjv_backup_view,
     unsigned int* backup_mutex_ptr,
     vecmem::data::vector_view<unsigned int> disjoint_set_view,
     vecmem::data::vector_view<unsigned int> cluster_size_view);
--- tests/hip/test_cca.cpp
+++ tests/hip/test_cca.cpp
@@ -1,6 +1,6 @@
 /** TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2022-2025 CERN for the benefit of the ACTS project
+ * (c) 2022-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -15,7 +15,7 @@
 #include "tests/cca_test.hpp"
 #include "traccc/clusterization/clustering_config.hpp"
 #include "traccc/clusterization/device/tags.hpp"
-#include "traccc/geometry/silicon_detector_description.hpp"
+#include "traccc/geometry/detector_design_description.hpp"
 #include "traccc/hip/clusterization/clusterization_algorithm.hpp"
 #include "traccc/hip/utils/stream.hpp"
 
@@ -24,13 +24,12 @@ vecmem::host_memory_resource host_mr;
 
 cca_function_t get_f_with(traccc::clustering_config cfg) {
     return [cfg](const traccc::edm::silicon_cell_collection::host& cells,
-                 const traccc::silicon_detector_description::host& dd)
+                 const traccc::detector_design_description::host& det_desc,
+                 const traccc::detector_conditions_description::host& det_cond)
                -> std::pair<std::map<traccc::geometry_id,
-                                     traccc::edm::measurement_collection<
-                                         traccc::default_algebra>::host>,
+                                     traccc::edm::measurement_collection::host>,
                             traccc::edm::silicon_cluster_collection::host> {
-        std::map<traccc::geometry_id, traccc::edm::measurement_collection<
-                                          traccc::default_algebra>::host>
+        std::map<traccc::geometry_id, traccc::edm::measurement_collection::host>
             geom_to_meas_map;
 
         traccc::hip::stream stream;
@@ -40,16 +39,33 @@ cca_function_t get_f_with(traccc::clustering_config cfg) {
         traccc::hip::clusterization_algorithm cc({device_mr, &host_mr}, copy,
                                                  stream, cfg);
 
-        traccc::silicon_detector_description::buffer dd_buffer{
+        traccc::detector_design_description::buffer det_descr_buffer{
+            [&]() {
+                std::vector<unsigned int> sizes(det_desc.size());
+                for (std::size_t i = 0; i < det_desc.size(); ++i) {
+                    auto this_design = det_desc.at(i);
+                    // now for each element, set the size to the largest size of
+                    // that element across all modules
+                    sizes[i] =
+                        std::max(static_cast<unsigned int>(
+                                     ((this_design.bin_edges_x()).size())),
+                                 static_cast<unsigned int>(
+                                     ((this_design.bin_edges_y()).size())));
+                }
+                return sizes;
+            }(),
+            device_mr, &host_mr, vecmem::data::buffer_type::fixed_size};
+        copy.setup(det_descr_buffer)->wait();
+        copy(vecmem::get_data(det_desc), det_descr_buffer)->wait();
+        traccc::detector_conditions_description::buffer det_cond_buffer{
             static_cast<
-                traccc::silicon_detector_description::buffer::size_type>(
-                dd.size()),
+                traccc::detector_conditions_description::buffer::size_type>(
+                det_cond.size()),
             device_mr};
-        copy.setup(dd_buffer)->ignore();
-        copy(vecmem::get_data(dd), dd_buffer,
+        copy.setup(det_cond_buffer)->ignore();
+        copy(vecmem::get_data(det_cond), det_cond_buffer,
              vecmem::copy::type::host_to_device)
             ->ignore();
-
         traccc::edm::silicon_cell_collection::buffer cells_buffer{
             static_cast<
                 traccc::edm::silicon_cell_collection::buffer::size_type>(
@@ -59,10 +75,9 @@ cca_function_t get_f_with(traccc::clustering_config cfg) {
         copy(vecmem::get_data(cells), cells_buffer)->wait();
 
         auto [measurements_buffer, cluster_buffer] =
-            cc(cells_buffer, dd_buffer,
+            cc(cells_buffer, det_descr_buffer, det_cond_buffer,
                traccc::device::clustering_keep_disjoint_set{});
-        traccc::edm::measurement_collection<traccc::default_algebra>::host
-            measurements{host_mr};
+        traccc::edm::measurement_collection::host measurements{host_mr};
         copy(measurements_buffer, measurements)->wait();
 
         traccc::edm::silicon_cluster_collection::host clusters{host_mr};
@@ -73,8 +88,7 @@ cca_function_t get_f_with(traccc::clustering_config cfg) {
                     measurements.at(i).surface_link().value()) == false) {
                 geom_to_meas_map.insert(
                     {measurements.at(i).surface_link().value(),
-                     traccc::edm::measurement_collection<
-                         traccc::default_algebra>::host{host_mr}});
+                     traccc::edm::measurement_collection::host{host_mr}});
             }
             geom_to_meas_map.at(measurements.at(i).surface_link().value())
                 .push_back(measurements.at(i));
