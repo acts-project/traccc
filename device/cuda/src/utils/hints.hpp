370648371563c048d98fab74db85b483bac33ba4
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2026-06-17 14:13:08 +0200

    Changed the logic of enabling/disabling CUDA register spilling.
    Since it seems that at least in some cases with CUDA 13 it doesn't
    actually work. So this allows users to turn it off by hand if
    necessary.

--- device/cuda/src/utils/hints.hpp
+++ device/cuda/src/utils/hints.hpp
@@ -1,14 +1,14 @@
 /**
  * TRACCC library, part of the ACTS project (R&D line)
  *
- * (c) 2024 CERN for the benefit of the ACTS project
+ * (c) 2024-2026 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
 
 #pragma once
 
-#if defined(__CUDA_ARCH__) && CUDART_VERSION >= 13000
+#if defined(__CUDA_ARCH__) && defined(TRACCC_CUDA_ENABLE_SPILL_TO_SHARED_MEMORY)
 #define TRACCC_CUDA_SPILL_TO_SHARED_MEMORY        \
     do {                                          \
         asm(".pragma \"enable_smem_spilling\";"); \
